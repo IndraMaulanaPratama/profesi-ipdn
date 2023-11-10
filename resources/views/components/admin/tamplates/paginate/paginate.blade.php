0bdc5593c26f984384eb035edb00652e8a176436
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-11-10 09:36:01 +0700

    UPDATE || Implement Responsive on paginate component

--- resources/views/components/admin/tamplates/paginate/paginate.blade.php
+++ resources/views/components/admin/tamplates/paginate/paginate.blade.php
@@ -1,5 +1,5 @@
 <div class="row">
-    <div class="col-4">
+    <div class="col-lg-4 col-md-6 col-sm-12">
         Showing {{ $item->firstItem() }} to {{ $item->lastItem() }} out of {{ $item->total() }} items
     </div>
     <div class="col-8 d-flex align-items-end flex-column">
