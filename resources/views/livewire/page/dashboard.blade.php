aaf85654b8911b3630beb131b4fba9f766720325
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-11-10 09:34:01 +0700

    UPDATE || Implement Responsive Page on Dashboard Page

--- resources/views/livewire/page/dashboard.blade.php
+++ resources/views/livewire/page/dashboard.blade.php
@@ -1,84 +1,83 @@
-<div class="">
-    <div class="row">
+<div class="row">
+    <div class="col-lg-8 col-md-6 col-sm-12">
+        <div class="row">
 
-        {{-- Card 1 --}}
-        <x-admin.components.card.card type='sales' title='Data Similaritas'>
+            {{-- Card 1 --}}
+            <x-admin.components.card.card size=8 type='sales' title='Dashboard 1'>
 
-            {{-- Dropdown Title --}}
-            <x-admin.components.card.dropdown-title title="pilihan">
-                <x-admin.components.card.dropdown-item text='Option 1' />
-            </x-admin.components.card.dropdown-title>
+                {{-- Dropdown Title --}}
+                <x-admin.components.card.dropdown-title title="pilihan">
+                    <x-admin.components.card.dropdown-item text='Option 1' />
+                </x-admin.components.card.dropdown-title>
 
-            {{-- Card Body --}}
-            <div class="d-flex align-items-center">
-                <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                    <i class="bi bi-percent"></i>
-                </div>
+                {{-- Card Body --}}
+                <div class="d-flex align-items-center">
+                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                        <i class="bi bi-percent"></i>
+                    </div>
 
-                <div class="ps-3">
-                    <h6>145</h6>
-                    <span class="text-success small pt-1 fw-bold">12%</span>
-                    <span class="text-muted small pt-2 ps-1">increase</span>
-                </div>
-            </div> <!-- End Of Card Body -->
+                    <div class="ps-3">
+                        <h6>145</h6>
+                        <span class="text-success small pt-1 fw-bold">12%</span>
+                        <span class="text-muted small pt-2 ps-1">increase</span>
+                    </div>
+                </div> <!-- End Of Card Body -->
 
-        </x-admin.components.card.card>
+            </x-admin.components.card.card>
 
-        {{-- Card Data B --}}
-        <x-admin.components.card.card type='revenue' title='Data B'>
+            {{-- Card 2 --}}
+            <x-admin.components.card.card size=4 type='sales' title='Dashboard 3'>
 
-            {{-- Dropdown Title --}}
-            <x-admin.components.card.dropdown-title title="pilihan">
-                <x-admin.components.card.dropdown-item text='Option 1' />
-                <x-admin.components.card.dropdown-item text='Option 2' />
-            </x-admin.components.card.dropdown-title>
+                {{-- Dropdown Title --}}
+                <x-admin.components.card.dropdown-title title="pilihan">
+                    <x-admin.components.card.dropdown-item text='Option 1' />
+                </x-admin.components.card.dropdown-title>
 
-            {{-- Card Body --}}
-            <div class="d-flex align-items-center">
-                <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                    <i class="bi bi-currency-dollar"></i>
-                </div>
-                <div class="ps-3">
-                    <h6>$3,264</h6>
-                    <span class="text-success small pt-1 fw-bold">8%</span> <span
-                        class="text-muted small pt-2 ps-1">increase</span>
+                {{-- Card Body --}}
+                <div class="d-flex align-items-center">
+                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                        <i class="bi bi-percent"></i>
+                    </div>
 
-                </div>
-            </div> <!-- End Of Card Body -->
+                    <div class="ps-3">
+                        <h6>145</h6>
+                        <span class="text-success small pt-1 fw-bold">12%</span>
+                        <span class="text-muted small pt-2 ps-1">increase</span>
+                    </div>
+                </div> <!-- End Of Card Body -->
 
-        </x-admin.components.card.card>
+            </x-admin.components.card.card>
 
-        {{-- Card Data C --}}
-        <x-admin.components.card.card type='customers' title='Data C' titleSpan='| Tahun ini'>
+            {{-- Card 3 --}}
+            <x-admin.components.card.card size=12 type='sales' title='Dashboard 3'>
 
-            {{-- Dropdown Title --}}
-            <x-admin.components.card.dropdown-title title="pilihan">
-                <x-admin.components.card.dropdown-item text='Option 1' />
-                <x-admin.components.card.dropdown-item text='Option 2' />
-                <x-admin.components.card.dropdown-item text='Option 3' />
-            </x-admin.components.card.dropdown-title>
+                {{-- Dropdown Title --}}
+                <x-admin.components.card.dropdown-title title="pilihan">
+                    <x-admin.components.card.dropdown-item text='Option 1' />
+                </x-admin.components.card.dropdown-title>
 
-            {{-- Card Body --}}
-            <div class="d-flex align-items-center">
-                <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                    <i class="bi bi-people"></i>
-                </div>
-                <div class="ps-3">
-                    <h6>1244</h6>
-                    <span class="text-danger small pt-1 fw-bold">12%</span> <span
-                        class="text-muted small pt-2 ps-1">decrease</span>
+                {{-- Card Body --}}
+                <div class="d-flex align-items-center">
+                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                        <i class="bi bi-percent"></i>
+                    </div>
 
-                </div>
-            </div> <!-- End Of Card Body -->
+                    <div class="ps-3">
+                        <h6>145</h6>
+                        <span class="text-success small pt-1 fw-bold">12%</span>
+                        <span class="text-muted small pt-2 ps-1">increase</span>
+                    </div>
+                </div> <!-- End Of Card Body -->
 
-        </x-admin.components.card.card>
+            </x-admin.components.card.card>
+
+        </div>
 
     </div>
 
-    <div class="row">
 
-        {{-- Card 1 --}}
-        <x-admin.components.card.card size=8 type='sales' title='Data Similaritas'>
+    <div class="col-lg-4 col-md-6 col-sm-12">
+        <x-admin.components.card.card size=12 type='sales' title='Dashboard 2'>
 
             {{-- Dropdown Title --}}
             <x-admin.components.card.dropdown-title title="pilihan">
@@ -86,42 +85,65 @@
             </x-admin.components.card.dropdown-title>
 
             {{-- Card Body --}}
-            <div class="d-flex align-items-center">
-                <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                    <i class="bi bi-percent"></i>
+            <div class="card-body">
+                <h5 class="card-title">Recent Activity <span>| Today</span></h5>
+
+                <div class="activity">
+
+                    <div class="activity-item d-flex">
+                        <div class="activite-label">32 min</div>
+                        <i class='bi bi-circle-fill activity-badge text-success align-self-start'></i>
+                        <div class="activity-content">
+                            Quia quae rerum <a href="#" class="fw-bold text-dark">explicabo officiis</a> beatae
+                        </div>
+                    </div><!-- End activity item-->
+
+                    <div class="activity-item d-flex">
+                        <div class="activite-label">56 min</div>
+                        <i class='bi bi-circle-fill activity-badge text-danger align-self-start'></i>
+                        <div class="activity-content">
+                            Voluptatem blanditiis blanditiis eveniet
+                        </div>
+                    </div><!-- End activity item-->
+
+                    <div class="activity-item d-flex">
+                        <div class="activite-label">2 hrs</div>
+                        <i class='bi bi-circle-fill activity-badge text-primary align-self-start'></i>
+                        <div class="activity-content">
+                            Voluptates corrupti molestias voluptatem
+                        </div>
+                    </div><!-- End activity item-->
+
+                    <div class="activity-item d-flex">
+                        <div class="activite-label">1 day</div>
+                        <i class='bi bi-circle-fill activity-badge text-info align-self-start'></i>
+                        <div class="activity-content">
+                            Tempore autem saepe <a href="#" class="fw-bold text-dark">occaecati voluptatem</a>
+                            tempore
+                        </div>
+                    </div><!-- End activity item-->
+
+                    <div class="activity-item d-flex">
+                        <div class="activite-label">2 days</div>
+                        <i class='bi bi-circle-fill activity-badge text-warning align-self-start'></i>
+                        <div class="activity-content">
+                            Est sit eum reiciendis exercitationem
+                        </div>
+                    </div><!-- End activity item-->
+
+                    <div class="activity-item d-flex">
+                        <div class="activite-label">4 weeks</div>
+                        <i class='bi bi-circle-fill activity-badge text-muted align-self-start'></i>
+                        <div class="activity-content">
+                            Dicta dolorem harum nulla eius. Ut quidem quidem sit quas
+                        </div>
+                    </div><!-- End activity item-->
+
                 </div>
 
-                <div class="ps-3">
-                    <h6>145</h6>
-                    <span class="text-success small pt-1 fw-bold">12%</span>
-                    <span class="text-muted small pt-2 ps-1">increase</span>
-                </div>
             </div> <!-- End Of Card Body -->
 
         </x-admin.components.card.card>
 
-        {{-- Card Data B --}}
-        <x-admin.components.card.card type='revenue' title='Data B'>
-
-            {{-- Dropdown Title --}}
-            <x-admin.components.card.dropdown-title title="pilihan">
-                <x-admin.components.card.dropdown-item text='Option 1' />
-                <x-admin.components.card.dropdown-item text='Option 2' />
-            </x-admin.components.card.dropdown-title>
-
-            {{-- Card Body --}}
-            <div class="d-flex align-items-center">
-                <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                    <i class="bi bi-currency-dollar"></i>
-                </div>
-                <div class="ps-3">
-                    <h6>$3,264</h6>
-                    <span class="text-success small pt-1 fw-bold">8%</span> <span
-                        class="text-muted small pt-2 ps-1">increase</span>
-
-                </div>
-            </div> <!-- End Of Card Body -->
-
-        </x-admin.components.card.card>
     </div>
 </div>
