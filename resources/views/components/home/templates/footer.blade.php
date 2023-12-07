c94465dbe1adbd7a0d79038ce3b99c6e5b8ab40f
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-12-07 18:32:26 +0700

    ADD NEW || Footer component for homepage

---
+++ resources/views/components/home/templates/footer.blade.php
@@ -0,0 +1,137 @@
+<div>
+    <hr style="color: grey;" class="shadow rounded" />
+
+    <div class="" style="padding: 40px 130px;">
+        <div class="row justify-content-between">
+
+            {{-- Title --}}
+            <div class="col-3">
+                <div class="row justify-content-between">
+                    <div class="col-11">
+                        <center>
+                            <img src="{{ asset('assets/homepage/images/logo.png') }}" alt="logo-ipdn" width="190px">
+                            <br />
+                            <div style="color: #0e416e;line-height: 1.2; padding:10px 0px">
+                                <b>PROGRAM PENDIDIKAN</b> <br />
+                                <b>PROFESI KEPAMONGPRAJAAN</b> <br />
+                                <small>INSTITUT PEMERINTAHAN DALAM NEGRI</small>
+                            </div>
+
+                        </center>
+                    </div>
+
+                    <div class="col-auto" style="margin: 50px 0;">
+                        <div class="rounded" style="border-left: 3px solid #eab940; height: 170px; "></div>
+                    </div>
+
+                </div>
+            </div>
+
+            {{-- Maps --}}
+            <div class="col-6">
+                <div class="row justify-content-between">
+                    <div class="col-11">
+                        <h6><b>SEKRETARIAT PROFESI KEPAMONGPRAJAAN IPDN KAMPUS JAKARTA</b></h6>
+                        <p style="font-size:12px">
+                            Jl. Ampera Raya, Kel. Cilandak Timur, Kota Jakarta Selatan, Kode Pos 12560 Telp./Fax. (021)
+                            7806602
+                        </p>
+
+                        <iframe
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3965.8440881537767!2d106.8158871749908!3d-6.28421639370468!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f21dff89fc03%3A0x7189e801feea4cf2!2sIPDN%20Kampus%20Jakarta!5e0!3m2!1sid!2sid!4v1701945881370!5m2!1sid!2sid"
+                            width="630" height="200" style="border:0;" allowfullscreen="" loading="lazy"
+                            referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
+
+                    <div class="col-auto" style="margin: 50px 0;">
+                        <div class="rounded" style="border-left: 3px solid #eab940; height: 170px; "></div>
+                    </div>
+                </div>
+            </div>
+
+            {{-- Kontak --}}
+            <div class="col-3">
+                <div class="row justify-content-between">
+                    <div class="col-11">
+                        <h6><b>HUBUNGI KAMI</b></h6>
+
+                        <div style="font-size:12px">
+                            <p>Bagian Kemahasiswaan</p>
+
+                            <table class="table table-borderless" style="line-height: 0.3">
+
+                                {{-- Phone 1 --}}
+                                <tr>
+                                    <td style="width: 1px">
+                                        <i class="bi bi-telephone"></i>
+                                    </td>
+
+                                    <td class="w-auto">
+                                        0858 2018 7794
+                                    </td>
+
+                                </tr>
+
+                                {{-- Phone 2 --}}
+                                <tr>
+                                    <td style="width: 1px">
+                                        &nbsp;
+                                    </td>
+
+                                    <td class="w-auto">
+                                        0812 1093 7771
+                                    </td>
+
+                                </tr>
+
+                                {{-- Email --}}
+                                <tr>
+                                    <td style="width: 1px">
+                                        <i class="bi bi-envelope"></i>
+                                    </td>
+
+                                    <td class="w-auto">
+                                        profesi.kepamongprajaan@ipdn.ac.id
+                                    </td>
+
+                                </tr>
+
+                                {{-- Instagram --}}
+                                <tr>
+                                    <td style="width: 1px">
+                                        <i class="bi bi-instagram"></i>
+                                    </td>
+
+                                    <td class="w-auto">
+                                        profesi_kepamongprajaan
+                                    </td>
+
+                                </tr>
+
+
+                                {{-- Tiktok --}}
+                                <tr>
+                                    <td style="width: 1px">
+                                        <i class="bi bi-tiktok"></i>
+                                    </td>
+
+                                    <td class="w-auto">
+                                        profesikepamongpr
+                                    </td>
+
+                                </tr>
+
+                            </table>
+
+                            <hr />
+                        </div>
+
+                        <a href="" class="link">Layanan Pengaduan</a>
+                    </div>
+
+                    <div class="col-auto">&nbsp;</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
