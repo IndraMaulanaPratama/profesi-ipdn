5ffe2c9de3826c241a930a060bf2b6d2b8b52ddf
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-12-07 18:35:01 +0700

    UPDATE || Removing middleware on dashboard route

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', Dashboard::class)->middleware('auth')->name('/');
+Route::get('/', Dashboard::class)->name('/');
 
 
 // Ranahna nu gaduh akses
