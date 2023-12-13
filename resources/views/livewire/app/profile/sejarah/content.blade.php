6854098acdc911aef4354166c1947af8bde43b72
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-12-13 10:05:19 +0700

    UPDATE || Content sejarah page

--- resources/views/livewire/app/profile/sejarah/content.blade.php
+++ resources/views/livewire/app/profile/sejarah/content.blade.php
@@ -1,19 +1,106 @@
 {{-- The Master doesn't talk, he acts. --}}
 
 <div>
+
+    {{-- Sejarah --}}
     <div class="container my-5">
-        <div class="row">
+        <div class="row justify-content-between">
 
             {{-- Sejarah --}}
-            <div class="col-8">
-                &nbsp;
+            <div class="col-7">
+                <div class="row">
+                    <div class="col-1">
+                        <img src="{{ asset('assets/homepage/images/activity.svg') }}" alt="sejarah-profesi"
+                            style="height: 8.2cm">
+                    </div>
+
+                    <div class="col-11">
+                        <h3 style="color: #CC2323">Tahun 2012</h3>
+
+                        <p style="text-align: justify">
+                            Dalam rangka meningkatkan tugas dan fungsi pendidikan IPDN untuk mengakomodasikan tuntutan
+                            kebutuhan
+                            kader pemerintahan yang profesional dan adaptif terhadap tuntutan kemajuan perkembangan ilmu
+                            pengetahuan dan teknologi sebagai dukungan terwujudnya pemerintahan yang demokratis,
+                            Direktur
+                            Jenderal Pendidikan Tinggi Kementerian Pendidikan Nasional mengeluarkan surat Mandat Nomor
+                            321/E/T/2012 tanggal 22 Februari Tahun 2012 yang ditujukan kepada Rektor IPDN dengan
+                            penugasan
+                            menyelenggarakan Program Profesi Kepamongprajaan. Surat mandat Dirjen Dikti ini dikukuhkan
+                            dalam
+                            Surat Keputusan Menteri Riset, Teknologi dan Pendidikan Tinggi Nomor 370/KPT/1/2018 tentang
+                            Penyelenggaraan Program Studi Profesi Kepamongprajaan pada IPDN.
+                        </p>
+
+
+                        <h3 style="color: #CC2323">Tahun 2014</h3>
+
+                        <p style="text-align: justify">
+                            Penerimaan mahasiswa baru Angkatan I pada Tahun 2014 sebanyak 38 orang yang berasal dari
+                            berbagai Provinsi, seperti Prov. Sulawesi Selatan, Prov. Sulawesi Utara, Prov. Bangka
+                            Belitung, Prov. Jambi, dan Prov. Papua.
+                        </p>
+
+                    </div>
+                </div>
             </div>
 
             {{-- Image --}}
             <div class="col-4">
-                <img src="https://thedogsnobs.com/wp-content/uploads/2021/12/golden-toy-poodle-face_.jpg" width="100%"
+                <img src="{{asset('assets/homepage/images/image 8.svg')}}" width="100%"
                     alt="">
             </div>
+        </div>
+    </div>
+
+    {{-- Tugas Pokok --}}
+    <div style="background-color: #efefef">
+        <div class="row justify-content-center">
+
+            <div class="col-6 my-5">
+                <h3 class="text-center mb-3" style="color: #003b6d"><b>TUGAS POKOK</b></h3>
+
+                <ol type="1">
+                    <li class=" mb-3">
+                        <p style="text-align: justify">
+                            Memberikan orientasi yang jelas pada bidang spesialisasi pemerintahan dalam rangka memenuhi
+                            kebutuhan pendidikan profesional dan akademik sesuai dengan pengembangan kader pemerintahan
+                            yang
+                            mempunyai kemampuan keilmuan dan keahlian praktis.
+                        </p>
+
+                        <center>
+                            <div class="rounded" style="border: 2px solid #eab940; height: 2px; width:80px;">&nbsp;
+                            </div>
+                        </center>
+                    </li>
+
+                    <li class="mb-3">
+                        <p style="text-align: justify">
+                            Meningkatkan peran dan fungsi lembaga pendidikan IPDN sebagai pusat pendidikan pemerintahan
+                            yang
+                            unggul dalam mendidik kader pimpinan pemerintahan sesuai dengan pengembangan Tri Dharma
+                            Perguruan Tinggi.
+                        </p>
+
+                        <center>
+                            <div class="rounded" style="border: 2px solid #eab940; height: 2px; width:80px;">&nbsp;
+                            </div>
+                        </center>
+
+                    </li>
+
+                    <li class="mb-3">
+                        <p style="text-align: justify">
+                            Memenuhi kepentingan dan kebutuhan akan kualitas aparatur pemerintahan sehubungan dengan
+                            pelaksanaan tugas dan fungsi pemerintahan yang mengimplementasikan otonomi daerah,
+                            akuntabilitas
+                            pemerintah yang produktif serta inovatif dalam pelayanan publik.
+                        </p>
+                    </li>
+                </ol>
+            </div>
+
         </div>
     </div>
 </div>
