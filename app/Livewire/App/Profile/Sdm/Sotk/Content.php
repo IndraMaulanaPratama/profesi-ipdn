236f5accd917fe99076c32acf8b9b663f155ea9f
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-12-13 09:12:59 +0700

    ADD NEW || Component Page SOTK

---
+++ app/Livewire/App/Profile/Sdm/Sotk/Content.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\App\Profile\Sdm\Sotk;
+
+use Livewire\Component;
+
+class Content extends Component
+{
+    public function render()
+    {
+        return view('livewire.app.profile.sdm.sotk.content');
+    }
+}
