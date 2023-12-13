2e59f26b7e8c3e34cdd568083f549da12d583522
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-12-13 10:18:23 +0700

    UPDATE || Content Homepage

--- resources/views/livewire/app/homepage/content.blade.php
+++ resources/views/livewire/app/homepage/content.blade.php
@@ -73,138 +73,175 @@
 
                 <div class="col-4">
                     <div class="card rounded-lg shadow">
-                        <img src="{{ asset('assets/homepage/images/bg-3.jpeg') }}" height="270" class="card-img-top"
-                            alt="...">
+
+                        <img src="{{ asset('assets/homepage/images/image 3.png') }}" height="270" class="card-img-top"
+                            alt="..." />
+
+                        {{-- Content --}}
                         <div class="card-body" style="height: 200px">
-                            <p class="card-text">
-                                Some quick example text to build on the card title and make up the
-                                bulk
-                                of the card's content.
+
+                            <h6>
+                                <b>
+                                    KEGIATAN PENGANUGERAHAN TANDA KARTIKA PAMONG PRAJA
+                                </b>
+                            </h6>
+
+                            <p class="" style="text-align: justify; font-size: 12px">
+                                Jatingagor, 27/10/2023 - Dalam rangka penganugerahan Tanda Kartika Pamong Praja dan
+                                Alumni Kehormatan Kepada Pejabat Gubernur Papua Barat Daya Dr. Drs. Mohammad Musa’ad,
+                                M.Si dan Bupati Malang Drs. H. Sanusi, M.M. sebagai bentuk apresiasi yang tinggi atas
+                                kontribusinya dalam mendayagunakan alumni Institut Pemerintahan Dalam Negeri di Provinsi
+                                Papua Barat Daya dan Kabupaten Malang.
+                            </p>
+                        </div>
+
+                    </div>
+                </div>
+
+
+                <div class="col-4">
+                    <div class="card rounded-lg shadow">
+                        <img src="{{ asset('assets/homepage/images/image 4.png') }}" height="270" class="card-img-top"
+                            alt="..." />
+
+                        {{-- Content --}}
+                        <div class="card-body" style="height: 200px">
+
+                            <h6>
+                                <b>
+                                    Provinsi Papua Barat Daya Tingkatkan SDM Kepala Distrik Melalui Pendidikan Profesi
+                                    Kepamongprajaan di IPDN -Kemendagri
+                                </b>
+                            </h6>
+
+                            <p class="" style="text-align: justify; font-size: 12px">
+                                Jakarta, 06/10/2023 - Rektor IPDN, Prof. Dr. Drs. H. Hadi Prabowo., M.M resmi membuka
+                                kegiatan matrikulasi dan perkuliahan program pendidikan profesi kepamongprajaan angkatan
+                                XII kelas bratha tahun akademik 2023/2024.
+                            </p>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="col-4">
+                    <div class="card rounded-lg shadow">
+                        <img src="{{ asset('assets/homepage/images/image 5.png') }}" height="270" class="card-img-top"
+                            alt="..." />
+
+                        {{-- Content --}}
+                        <div class="card-body" style="height: 200px">
+
+                            <h6>
+                                <b>
+                                    PENGANUGERAHAN TANDA KEHORMATAN DAM LENCANA ALUMNI KEHORMATAN
+                                </b>
+                            </h6>
+
+                            <p class="" style="text-align: justify; font-size: 12px">
+                                Jatingagor, 04/03/2023 - Rektor IPDN, Prof. Dr. Drs. H. Hadi Prabowo., M.M memberikan
+                                penganugerahan tanda kehormatan Kartika Pamong Praja Madya, Kartika Pamong Praja Muda
+                                dan Lencana Alumni Kehormatan Pendidikan Tinggi Kepamongprajaan kepada Direktur Program
+                                Pendidikan Profesi Kepamongprajaan Dr. Dra. Hj. Endang Try Setyasih.
                             </p>
                         </div>
                     </div>
+
                 </div>
 
-
-                <div class="col-4">
-                    <div class="card rounded-lg shadow">
-                        <img src="{{ asset('assets/homepage/images/bg-2.jpeg') }}" height="270" class="card-img-top"
-                            alt="...">
-                        <div class="card-body" style="height: 200px">
-                            <p class="card-text">Some quick example text to build on the card title and make up the
-                                bulk
-                                of the card's content.</p>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-4">
-                    <div class="card rounded-lg shadow">
-                        <img src="https://wallpapercave.com/wp/wp7614917.jpg" height="270" class="card-img-top"
-                            alt="...">
-                        <div class="card-body" style="height: 200px">
-                            <p class="card-text">Some quick example text to build on the card title and make up the
-                                bulk
-                                of the card's content.</p>
-                        </div>
-                    </div>
+                {{-- See More --}}
+                <div class="py-4">
+                    <center>
+                        <a href="" class="link">Lihat Selengkapnya ...</a>
+                    </center>
                 </div>
 
             </div>
 
-            {{-- See More --}}
-            <div class="py-4">
+        </div>
+
+        {{-- Apa Kata Mahasiswa/Alumni --}}
+        <div class="row py-1">
+
+            <div class="col-12 py-4">
+
+                {{-- Title --}}
                 <center>
-                    <a href="" class="link">Lihat Selengkapnya ...</a>
+                    <h3><b>APA KATA MAHASISWA/ALUMNI</b></h3>
                 </center>
-            </div>
-
-        </div>
-
-    </div>
-
-    {{-- Apa Kata Mahasiswa/Alumni --}}
-    <div class="row py-1">
-
-        <div class="col-12 py-4">
-
-            {{-- Title --}}
-            <center>
-                <h3><b>APA KATA MAHASISWA/ALUMNI</b></h3>
-            </center>
 
 
-            {{-- Card Berita --}}
-            <div class="row justify-content-evenly my-4">
-                <div class="col-12 p-4 rounded" style="background-color: #003b6d">
+                {{-- Card Berita --}}
+                <div class="row justify-content-evenly my-4">
+                    <div class="col-12 p-4 rounded" style="background-color: #003b6d">
 
-                    <div class="row align-items-start">
+                        <div class="row align-items-start">
 
-                        <div class="col-6">
-                            <div class="row">
-                                <div class="col-auto">
-                                    <img src="https://images.unsplash.com/photo-1517423440428-a5a00ad493e8?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTJ8fHxlbnwwfHx8fHw%3D"
-                                        alt="mahasiswa-1" class="rounded img" width="100" height="100" />
-                                </div>
+                            <div class="col-6">
+                                <div class="row">
+                                    <div class="col-auto">
+                                        <img src="https://i.pinimg.com/originals/32/a0/31/32a031cddeb9c1649062a13aa946c8c0.jpg"
+                                            alt="mahasiswa-1" class="rounded img" width="100" height="100" />
+                                    </div>
 
-                                <div class="col-8">
-                                    <h1><i class="bi bi-quote" style="color: orange"></i></h1>
+                                    <div class="col-8">
+                                        <h1><i class="bi bi-quote" style="color: orange"></i></h1>
 
-                                    <p class="px-3" style="color: whitesmoke; text-align: justify;">
-                                        Terbaik!! <br />
-                                        Developer aplikasi ini sangat bahagia dan tercukupi kehidupanya. Selain itu
-                                        tempat
-                                        dia bekerja memiliki struktural yang sangat baik, supportif, mengedepankan
-                                        work life
-                                        balance, memperhatikan kesehatan dan kesejahteraan pegawai-pegawainya.
-                                    </p>
+                                        <p class="px-3" style="color: whitesmoke; text-align: justify;">
+                                            Terbaik!! <br />
+                                            Developer aplikasi ini sangat bahagia dan tercukupi kehidupanya. Selain itu
+                                            tempat
+                                            dia bekerja memiliki struktural yang sangat baik, supportif, mengedepankan
+                                            work life
+                                            balance, memperhatikan kesehatan dan kesejahteraan pegawai-pegawainya.
+                                        </p>
 
-                                    <p class="px-3" style="color: orange; font-size: 13px">
-                                        Mark Zulkarnain <br />
-                                        Angkatan XXI <br />
-                                        Regional Baghdad
-                                    </p>
+                                        <p class="px-3" style="color: orange; font-size: 13px">
+                                            Mark Zulkarnain <br />
+                                            Angkatan XXI <br />
+                                            Regional Baghdad
+                                        </p>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
 
 
-                        <div class="col-6">
-                            <div class="row">
-                                <div class="col-auto">
-                                    <img src="https://i.pinimg.com/originals/32/a0/31/32a031cddeb9c1649062a13aa946c8c0.jpg"
-                                        alt="mahasiswa-1" class="rounded img" width="100" height="100" />
-                                </div>
+                            <div class="col-6">
+                                <div class="row">
+                                    <div class="col-auto">
+                                        <img src="https://i.pinimg.com/originals/32/a0/31/32a031cddeb9c1649062a13aa946c8c0.jpg"
+                                            alt="mahasiswa-1" class="rounded img" width="100" height="100" />
+                                    </div>
 
-                                <div class="col-8">
-                                    <h1><i class="bi bi-quote" style="color: orange"></i></h1>
+                                    <div class="col-8">
+                                        <h1><i class="bi bi-quote" style="color: orange"></i></h1>
 
-                                    <p class="px-3" style="color: whitesmoke; text-align: justify;">
-                                        Terbaik!! <br />
-                                        Developer aplikasi ini sangat bahagia dan tercukupi kehidupanya. Selain itu
-                                        tempat
-                                        dia bekerja memiliki struktural yang sangat baik, supportif, mengedepankan
-                                        work life
-                                        balance, memperhatikan kesehatan dan kesejahteraan pegawai-pegawainya.
-                                    </p>
+                                        <p class="px-3" style="color: whitesmoke; text-align: justify;">
+                                            Terbaik!! <br />
+                                            Developer aplikasi ini sangat bahagia dan tercukupi kehidupanya. Selain itu
+                                            tempat
+                                            dia bekerja memiliki struktural yang sangat baik, supportif, mengedepankan
+                                            work life
+                                            balance, memperhatikan kesehatan dan kesejahteraan pegawai-pegawainya.
+                                        </p>
 
-                                    <p class="px-3" style="color: orange; font-size: 13px">
-                                        Ma'ruf Amir Aswadji <br />
-                                        Angkatan CGV <br />
-                                        Regional Rohingya
-                                    </p>
+                                        <p class="px-3" style="color: orange; font-size: 13px">
+                                            Ma'ruf Amir Aswadji <br />
+                                            Angkatan CGV <br />
+                                            Regional Rohingya
+                                        </p>
 
+                                    </div>
                                 </div>
                             </div>
+
                         </div>
 
                     </div>
-
                 </div>
+
             </div>
 
         </div>
 
     </div>
-
-</div>
