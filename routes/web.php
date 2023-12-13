7983661b48a405ba59424f33139e0327613efac9
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-12-13 09:14:56 +0700

    UPDATE || web route

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,12 @@
 use App\Livewire\Page\Admin\Assign;
 use App\Livewire\Page\Admin\Role;
 use App\Livewire\Page\Admin\Users;
-use App\Livewire\Page\Dashboard;
+use App\Livewire\Page\App\Dashboard;
+use App\Livewire\Page\App\LayananPengaduan;
+use App\Livewire\Page\App\Profile\Sdm\Sotk;
+use App\Livewire\Page\App\Profile\Sejarah;
+use App\Livewire\Page\App\ProfileDeskripsi;
+use App\Livewire\Page\App\ProfileVisiMisi;
 use App\Livewire\Page\Login;
 use App\Livewire\Page\Menu;
 use Illuminate\Support\Facades\Auth;
@@ -11,6 +16,13 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', Dashboard::class)->name('/');
+Route::get('/profile/layanan-pengaduan', LayananPengaduan::class)->name('layanan-pengaduan');
+
+// Profile Menu
+Route::get('/profile/deskripsi', ProfileDeskripsi::class)->name('profile-deskripsi');
+Route::get('/profile/visi-dan-misi', ProfileVisiMisi::class)->name('profile-visimisi');
+Route::get('/profile/simber-daya-manusia/struktur-organisasi-tenaga-kependidikan', Sotk::class)->name('profile-sotk');
+Route::get('/profile/sejarah', Sejarah::class)->name('profile-sejarah');
 
 
 // Ranahna nu gaduh akses
