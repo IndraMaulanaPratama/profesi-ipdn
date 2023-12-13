2e176e0b5abeba19495e0fccfb3575b16ebda3d7
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-12-13 09:09:15 +0700

    ADD NEW || Page sotk app

---
+++ app/Livewire/Page/App/Profile/Sdm/Sotk.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Livewire\Page\App\Profile\Sdm;
+
+use Livewire\Attributes\Title;
+use Livewire\Component;
+
+#[Title('Profesi - Struktur Organisasi')]
+class Sotk extends Component
+{
+    public function render()
+    {
+        return view('livewire.page.app.profile.sdm.sotk');
+    }
+}
---
+++ resources/views/livewire/page/app/profile/sdm/sotk.blade.php
@@ -0,0 +1,12 @@
+{{-- Stop trying to control. --}}
+
+
+<div>
+    {{-- Header --}}
+    @livewire('App.Profile.sdm.sotk.Header', [], key('header'))
+
+
+    {{-- Content --}}
+    @livewire('App.Profile.sdm.sotk.Content', [], key('content'))
+
+</div>
