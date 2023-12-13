e1ee22c7039786c1c6667df80ac842fd26852b35
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-12-13 09:12:39 +0700

    ADD NEW || Component Page Deskripsi

---
+++ resources/views/livewire/app/profile/deskripsi/content.blade.php
@@ -0,0 +1,204 @@
+{{-- If your happiness depends on money, you will never be happy with yourself. --}}
+
+<div>
+
+    {{-- Deskripsi --}}
+    <div class="container my-5">
+        <div class="row justify-content-between">
+
+            {{-- Narasi --}}
+            <div class="col-5">
+                <p style="text-align: justify; text-indent: 1cm">
+                    Dalam rangka menyiapkan sumber daya manusia aparatur yang profesional di bidang
+                    Pemerintahan, serta menimbang bahwa IPDN sebagai Lembaga Pendidikan tinggi kedinasan yang
+                    menyelenggarakan program Pendidikan di bidang Pemerintahan khususnya Program Pendidikan Profesi
+                    Kepamongprajaan, maka Menteri Riset Teknologi dan Pendidikan Tinggi Mengeluarkan Keputusan Nomor
+                    370/KPT/I/2018 tentang Izin Pembukaan Program Studi Pendidikan Profesi Kepamongprajaan Program
+                    Profesi
+                    pada Institut Pemerintahan Dalam Negeri di Kabupaten Sumedang yang diselenggarakan Oleh Kementerian
+                    Dalam Negeri.
+                </p>
+
+                <p style="text-align: justify; text-indent: 1cm">
+                    Pamong Praja adalah aparatur penyelenggara Pemerintahan Dalam Negeri yang melaksanakan tugas
+                    pembinaan ketentraman dan ketertiban, koordinasi penyelenggaraan Pemerintahan, serta pembinaan
+                    kesatuan bangsa dan politik dalam negeri.
+                </p>
+            </div>
+
+            {{-- Gambar --}}
+            <div class="col-6">
+                <img src="https://static.promediateknologi.id/crop/0x0:0x0/0x0/webp/photo/p2/19/2023/07/10/dog-g3d9a3a2c1_1280-639541513.jpg"
+                    width="80%" alt="">
+            </div>
+
+        </div>
+    </div>
+
+    {{-- Pengertian Perbedaan --}}
+    <div class="p-3" style="background-image: linear-gradient(#ebf5fe, #f7f1f5);">
+        <div class="container">
+
+            {{-- Pamong Praja dan Kepamongprajaan --}}
+            <div class="row">
+
+                {{-- Pamong Praja --}}
+                <div class="col-5 m-4 p-4 rounded shadow" style="background-color: #003b6d; color: whitesmoke;">
+
+                    <h5 style="color: #fbd679">PAMONG PRAJA</h5>
+                    <br />
+                    <p class="paragraf">
+                        Pamong Praja adalah aparatur penyelenggara Pemerintahan Dalam Negeri yang melaksanakan tugas
+                        pembinaan ketentraman dan ketertiban, koordinasi penyelenggaraan Pemerintahan, serta pembinaan
+                        kesatuan bangsa dan politik dalam negeri.
+                    </p>
+                </div>
+
+                {{-- Kepamongprajaan --}}
+                <div class="col-5 m-4 p-4 rounded shadow"
+                    style="background-color: #003b6d; color: whitesmoke; text-align: justify">
+
+                    <h5 style="color: #fbd679">KEPAMONGPRAJAAN</h5>
+                    <br />
+                    <p class="paragraf">
+                        Kepamongprajaan adalah sistem nilai yang menunjukan kondisi dan karakter mengenai pamong praja.
+                    </p>
+                </div>
+            </div>
+
+            {{-- Profesi Kepamongprajaan --}}
+            <div class="row my-4 justify-content-center">
+                <div class="col-8 p-4 border bg-white rounded shadow">
+
+                    {{-- Title Section --}}
+                    <h5 class="text-center" style="color: #003b6d;">
+                        <b>PROFESI KEPAMONGPRAJAAN</b>
+                    </h5>
+
+                    <p class="paragraf pt-4">
+                        Profesi Kepamongprajaan adalah keahlian aparatur Pemerintahan dalam menerapkan atau
+                        mengaplikasikan ilmu pengetahuan, etika, teknologi, seni atau keterampilan dan nilai-nilai
+                        lainnya dalam penyelenggaraan Pemerintahan Dalam Negeri, Kode Etik Kepamongprajaan, dimana
+                        “Korps Pamong Praja” memiliki kode etik atau Code of Conduct yang dinamakan HASTA BUDI BHAKTI
+                        PAMONG PRAJA yaitu Delapan Nilai Pegangan Untuk Berbakti yang mengandung nilai profesional dan
+                        etika.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    {{-- Kode Etik --}}
+    <div class="container my-5">
+
+        {{-- Title Section --}}
+        <h2 class="text-center" style="color: #003b6d;">
+            <b>8 KODE ETIK KEPAMONGPRAJAAN</b>
+        </h2>
+
+
+        {{-- 8 Kode Etik --}}
+        <div>
+            {{-- 1 - 3 --}}
+            <div class="row justify-content-evenly m-5">
+
+                {{-- 1 --}}
+                <div class="col-3 p-4 rounded shadow">
+                    <h5>1</h5>
+
+                    <p class="paragraf-etik">
+                        Korps Pamong Praja Sebagai pengamal Pancasila dan pembela Negara Kesatuan Republik Indonesia
+                        menjadi
+                        pengayom dari seluruh rakyat tanpa membedakan golongan, aliran dan agama
+                    </p>
+                </div>
+
+                {{-- 2 --}}
+                <div class="col-3 p-4 rounded shadow">
+                    <h5>2</h5>
+
+                    <p class="paragraf-etik">
+                        Korps Pamong Praja berkewajiban memberikan petunjuk dan bimbingan kepada rakyat dalam pergaulan
+                        hidup Bersama menuju ketertiban dan ketentraman umum
+                    </p>
+                </div>
+
+                {{-- 3 --}}
+                <div class="col-3 p-4 rounded shadow">
+                    <h5>3</h5>
+
+                    <p class="paragraf-etik">
+                        Korps Pamong Praja merupakan penyuluh dalam gelap dan menolong dalam penderitaan bagi seluruh
+                        lapisan masyarakat sehingga tercapai ketenangan dan ketentraman lahir bathin
+                    </p>
+                </div>
+            </div>
+
+            {{-- 4 - 6 --}}
+            <div class="row justify-content-evenly m-5">
+
+                {{-- 4 --}}
+                <div class="col-3 p-4 rounded shadow">
+                    <h5>4</h5>
+
+                    <p class="paragraf-etik">
+                        Korps Pamong Praja membina semangat kehidupan masyarakat sehingga terjelma sifat dan sikap
+                        dinamis, konstruktif, korektif
+                    </p>
+                </div>
+
+                {{-- 5 --}}
+                <div class="col-3 p-4 rounded shadow">
+                    <h5>5</h5>
+
+                    <p class="paragraf-etik">
+                        Korps Pamong Praja bertugas menumbuhkan dan memupuk daya cipta rakyat ke arah kesejahteraan
+                        masyarakat.
+                    </p>
+                </div>
+
+                {{-- 6 --}}
+                <div class="col-3 p-4 rounded shadow">
+                    <h5>6</h5>
+
+                    <p class="paragraf-etik">
+                        Korps Pamong Praja bertugas menampung dan mencarikan penyelesaian segala persoalan hidup dan
+                        kehidupan sehari-hari sehingga diperlukan sifat sabar, tekun, ulet dan bijaksana
+                    </p>
+                </div>
+            </div>
+
+            {{-- 7 - 8 --}}
+            <div class="row justify-content-evenly m-5">
+
+                {{-- 7 --}}
+                <div class="col-3 p-4 rounded shadow">
+                    <h5>7</h5>
+
+                    <p class="paragraf-etik">
+                        Korps Pamong Praja menjadi penggerak segala kegiatan dalam masyarakat menuju tercapainya
+                        masyarakat yang adil dan Makmur yang disertai oleh Tuhan Yang Maha Esa
+                    </p>
+                </div>
+
+                {{-- 8 --}}
+                <div class="col-3 p-4 rounded shadow">
+                    <h5>8</h5>
+
+                    <p class="paragraf-etik">
+                        Korps Pamong Praja Harus bertindak tegas, adil dan jujur dalam memberantas kejahatan dan
+                        kemaksiatan tanpa pandang bulu, sebaliknya menjadi teladan dalam kebaikan dan kemaslahatan
+                    </p>
+                </div>
+
+                {{--  --}}
+                <div class="col-3 p-4">
+                    &nbsp
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+
+</div>
---
+++ resources/views/livewire/app/profile/deskripsi/header.blade.php
@@ -0,0 +1,44 @@
+{{-- Close your eyes. Count to one. That is how long forever feels. --}}
+
+<div>
+
+    {{-- Breadcrumb --}}
+    <div class="container mb-3" style="font-size: 12px;">
+        <a href="{{ route('/') }}" class="link" style="">
+            Beranda
+        </a>
+
+        &nbsp; &#62; &nbsp;
+
+        <a href="#">
+            Profile
+        </a>
+
+        &nbsp; &#62; &nbsp;
+
+        <a href="{{ route('profile-deskripsi') }}">
+            Apa itu pendidikan kepamongprajaan
+        </a>
+    </div>
+
+    {{-- Jumbotron --}}
+    <div class="row">
+        <div class="col-12">
+            <!-- Jumbotron -->
+            <div class="text-center bg-image"
+                style="background-image: url('https://mdbcdn.b-cdn.net/img/new/slides/041.webp');height: auto;">
+
+                <div class="mask" style="background-color: rgba(0, 0, 0, 0.6);">
+                    <div class="d-flex justify-content-center align-items-center" style=" height: 200px">
+                        <div style="color: #ffda79;">
+                            <h1 class=""><b>APA ITU PENDIDIKAN KEPAMONGPRAJAAN</b></h1>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <!-- Jumbotron -->
+        </div>
+    </div>
+
+</div>
