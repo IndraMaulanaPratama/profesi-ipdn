35b39990057a93496ba799dd41c73274b9c4eb8e
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-12-13 12:30:46 +0700

    UPDATE || Content Layanan Pengaduan

--- resources/views/livewire/app/layanan-pengaduan/content.blade.php
+++ resources/views/livewire/app/layanan-pengaduan/content.blade.php
@@ -1,4 +1,150 @@
 {{-- Do your work, then step back. --}}
 
-<div>
+<style>
+    .optional {
+        color: #5B5B5B;
+        font-size: 13px;
+        font-style: italic;
+        padding-left: 5px;
+    }
+</style>
+
+
+<div style="background-color: #ecf6ff;">
+
+    <div class="row py-5 justify-content-center">
+        <div class="col-8 shadow-sm rounded-sm" style="background-color:white;">
+
+            <div class="row justify-content-between">
+                <div class="col-5 px-5 py-4">
+                    {{-- Email --}}
+                    <div class="py-2">
+                        <label for="exampleFormControlInput1" class="form-label">E-mail <small
+                                class="optional">(Optional)</small></label>
+                        <input type="email" class="form-control">
+                    </div>
+
+                    {{-- Nama --}}
+                    <div class="py-2">
+                        <label for="exampleFormControlInput1" class="form-label">Nama <small
+                                class="optional">(Optional)</small></label>
+                        <input type="text" class="form-control">
+                    </div>
+
+                    {{-- Nama --}}
+                    <div class="py-2">
+                        <label for="exampleFormControlInput1" class="form-label">Pengaduan</label>
+                        <textarea name="" class="form-control" rows="5"></textarea>
+                    </div>
+
+                    <div class="py-2">
+                        <button class="btn btn-outline-secondary">Simpan</button>
+                    </div>
+                </div>
+
+                <div class="col-6 border" style="padding: 0%">
+                    <!-- Jumbotron -->
+                    <div class="bg-image"
+                        style="background-image: url('https://eskipaper.com/images/large-2.jpg');height: 100%; width: 100%">
+
+                        <div class="mask" style="background-color: rgba(0, 0, 0, 0.6); height:100%">
+                            <div class=" p-5" style=" height: 100%; color:whitesmoke">
+                                <h3><b>HUBUNGI KAMI</b></h3>
+                                <br/>
+
+                                <table width="100%" style="line-height: 2.3; font-size:13px;">
+
+                                    <tr>
+                                        <td colspan="3">
+                                            Bagian Kemahasiswaan
+                                        </td>
+                                    </tr>
+
+                                    {{-- Phone 1 --}}
+                                    <tr>
+                                        <td style="width: 30px">
+                                            <i class="bi bi-telephone"></i>
+                                        </td>
+
+                                        <td class="w-auto">
+                                            0858 2018 7794
+                                        </td>
+
+                                    </tr>
+
+                                    {{-- Phone 2 --}}
+                                    <tr>
+                                        <td style="width: 30px">
+                                            &nbsp;
+                                        </td>
+
+                                        <td class="w-auto">
+                                            0812 1093 7771
+                                        </td>
+
+                                    </tr>
+
+                                    {{-- Email --}}
+                                    <tr>
+                                        <td style="width: 30px">
+                                            <i class="bi bi-envelope"></i>
+                                        </td>
+
+                                        <td class="w-auto">
+                                            profesi.kepamongprajaan@ipdn.ac.id
+                                        </td>
+
+                                    </tr>
+
+                                    <tr>
+                                        <td colspan="3">
+                                            <hr/>
+                                        </td>
+                                    </tr>
+
+
+                                    <tr>
+                                        <td colspan="3">
+                                            Social Media
+                                        </td>
+                                    </tr>
+
+                                    {{-- Instagram --}}
+                                    <tr>
+                                        <td style="width: 30px">
+                                            <i class="bi bi-instagram"></i>
+                                        </td>
+
+                                        <td class="w-auto">
+                                            profesi_kepamongprajaan
+                                        </td>
+
+                                    </tr>
+
+
+                                    {{-- Tiktok --}}
+                                    <tr>
+                                        <td style="width: 30px">
+                                            <i class="bi bi-tiktok"></i>
+                                        </td>
+
+                                        <td class="w-auto">
+                                            profesikepamongpr
+                                        </td>
+
+                                    </tr>
+
+                                </table>
+
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+    </div>
 </div>
