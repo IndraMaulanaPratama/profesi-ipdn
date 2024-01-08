562dce6ad06728ef28e1db42799e86343860769e
IndraMaulanaPratama <maulana.indra808@gmail.com>
2024-01-08 17:57:48 +0700

    css ga kebaca

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 
-    @vite(['/resources/css/app.css', '/resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
 
 </head>
