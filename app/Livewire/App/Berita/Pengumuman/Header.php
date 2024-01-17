a0e91c81f0d5322cd0c33a9845de3b3430266db0
IndraMaulanaPratama <maulana.indra808@gmail.com>
2024-01-17 11:23:25 +0700

    ADD NEW || HEADER For Pengumuman

---
+++ app/Livewire/App/Berita/Pengumuman/Header.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\App\Berita\Pengumuman;
+
+use Livewire\Component;
+
+class Header extends Component
+{
+    public function render()
+    {
+        return view('livewire.app.berita.pengumuman.header');
+    }
+}
