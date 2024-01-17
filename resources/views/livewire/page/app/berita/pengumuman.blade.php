79efc33ab7f5d4f4af193f4b829caa247a6f317b
IndraMaulanaPratama <maulana.indra808@gmail.com>
2024-01-17 11:22:22 +0700

    ADD NEW || Page for Pengumuman

---
+++ resources/views/livewire/page/app/berita/pengumuman.blade.php
@@ -0,0 +1,12 @@
+{{-- In work, do what you enjoy. --}}
+
+<div>
+
+    {{-- Header --}}
+    @livewire('App.berita.pengumuman.header', [], key('header'))
+
+
+    {{-- Content --}}
+    @livewire('App.berita.pengumuman.content', [], key('content'))
+
+</div>
