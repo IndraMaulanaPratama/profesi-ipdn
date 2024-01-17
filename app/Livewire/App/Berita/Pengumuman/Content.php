92154571132c99b5b8c7afd05efb07a78026bd2a
IndraMaulanaPratama <maulana.indra808@gmail.com>
2024-01-17 11:22:46 +0700

    ADD NEW || Content Berita

---
+++ app/Livewire/App/Berita/Pengumuman/Content.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\App\Berita\Pengumuman;
+
+use Livewire\Component;
+
+class Content extends Component
+{
+    public function render()
+    {
+        return view('livewire.app.berita.pengumuman.content');
+    }
+}
---
+++ resources/views/livewire/app/berita/pengumuman/content.blade.php
@@ -0,0 +1,94 @@
+{{-- Do your work, then step back. --}}
+
+<div class="container">
+
+    {{-- Banner --}}
+    <div style="padding: 30px 0px;">
+        <center>
+            <img src="{{ asset('assets/homepage/images/PENERIMAAN MAHASISWA BARU 1.png') }}" width="80%" />
+        </center>
+    </div>
+
+    {{-- Title Section --}}
+    <div style="padding:30px 0px; ">
+        <h4 style="color: #063A69; font-weight: bold">PENERIMAAN MAHASISWA BARU 2024</h4>
+        <div class="rounded" style="border: 3px solid #eab940; height: 5px; width:80px; background-color: #eab940 "></div>
+    </div>
+
+    {{-- Content Card --}}
+
+    <div class="row g-4 my-5">
+
+        {{-- Pengumuman 1 --}}
+        <div class="col-3">
+            <div class="card" style="width: 18rem;">
+                {{-- Image Preview --}}
+                <img src="{{ asset('assets/homepage/images/Brosur Profesi Kepamongprajaan.png') }}" class="card-img-top"
+                    alt="Image Card 1" height="150px" />
+
+
+                {{-- Text Content --}}
+                <div class="card-body" style="font-weight: bold">
+                    <a href="https://drive.google.com/file/d/1EQhYXdH-yXyrbWxO2VIwhkrqBiRCDPFe/view?usp=drive_link"
+                        target="_">
+                        <p class="card-text">Brosur Profesi Kepamongprajaan.</p>
+                    </a>
+                </div>
+            </div>
+
+        </div>
+
+        {{-- Pengumuman 2 --}}
+        <div class="col-3">
+            <div class="card" style="width: 18rem;">
+                {{-- Image Preview --}}
+                <img src="{{ asset('assets/homepage/images/Surat Edaran Menteri Dalam Negeri.png') }}" class="card-img-top"
+                    alt="Image Card 1" height="150px" />
+
+
+                {{-- Text Content --}}
+                <div class="card-body" style="font-weight: bold">
+                    <a href="https://drive.google.com/file/d/13NkoA8Ndrix77qIu4ghKOd3PX6_RSheD/view?usp=drive_link"
+                        target="_">
+                        <p class="card-text">Surat Edaran Menteri Dalam Negeri.</p>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        {{-- Pengumuman 3 --}}
+        <div class="col-3">
+            <div class="card" style="width: 18rem;">
+                {{-- Image Preview --}}
+                <img src="{{ asset('assets/homepage/images/Surat Rektor Penerimaan Mahasiswa Baru Ak. XIII.png') }}" class="card-img-top"
+                    alt="Image Card 1" height="150px" />
+
+
+                {{-- Text Content --}}
+                <div class="card-body" style="font-weight: bold">
+                    <a href="https://drive.google.com/file/d/1qpy-DPEyRsqV4OhW_iIbq7lEsIZAJWu0/view?usp=drive_link"
+                        target="_">
+                        <p class="card-text">Surat Rektor Penerimaan Mahasiswa Baru Ak. XIII.</p>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        {{-- Pengumuman 4 --}}
+        <div class="col-3">
+            <div class="card" style="width: 18rem;">
+                {{-- Image Preview --}}
+                <img src="{{ asset('assets/homepage/images/PENERIMAAN MAHASISWA BARU 1.png') }}" class="card-img-top"
+                    alt="Image Card 1" height="150px" />
+
+
+                {{-- Text Content --}}
+                <div class="card-body" style="font-weight: bold">
+                    <p class="card-text">Seleksi Penerimaan Mahasiswa Baru Ak. XIII.</p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+</div>
