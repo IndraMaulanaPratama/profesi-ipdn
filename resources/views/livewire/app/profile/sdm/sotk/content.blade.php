e3f7891a404290855be9a32e068669bc702532de
IndraMaulanaPratama <maulana.indra808@gmail.com>
2024-01-25 11:11:35 +0700

    benerin jarak jabatan

--- resources/views/livewire/app/profile/sdm/sotk/content.blade.php
+++ resources/views/livewire/app/profile/sdm/sotk/content.blade.php
@@ -7,30 +7,35 @@
 
                 {{-- Dr. Drs. H. Endang Try Setyasih, MM --}}
                 <div class="col-6 text-center">
-                    <img src="{{ asset('assets/homepage/images/Dr. Drs. H. Endang Try Setyasih, MM.png') }}" width="251px" height="276px" alt="" srcset="">
+                    <img src="{{ asset('assets/homepage/images/Dr. Drs. H. Endang Try Setyasih, MM.png') }}"
+                        width="251px" height="276px" alt="" srcset="">
 
-                    <p class="text-center pt-2" style="font-weight: bold; font-size: 24px; line-height: normal">
-                        Dr. Drs. H. Endang Try Setyasih, MM
-                    </p>
-                    <p style="color: #063A69; font-size: 16px; font-weight: 700">
-                        Direktur
-                    </p>
+                    <div class="text-center pt-2">
+                        <div class="pb-2" style="font-weight: bold; font-size: 24px; line-height: normal;">
+                            Dr. Drs. H. Endang Try Setyasih, MM
+                        </div>
 
+                        <div style="color: #063A69; font-size: 16px; font-weight: 700">
+                            Direktur
+                        </div>
+                    </div>
                 </div>
 
                 {{-- Drs. Komaedi, M.Si --}}
                 <div class="col-6 text-center">
-                    <img src="{{ asset('assets/homepage/images/Drs. Komaedi, M.Si.png') }}"  width="251px" height="276px"
-                        alt="" srcset="">
+                    <img src="{{ asset('assets/homepage/images/Drs. Komaedi, M.Si.png') }}" width="251px"
+                        height="276px" alt="" srcset="">
 
-                    <p class="text-center pt-2" style="font-weight: bold; font-size: 24px; line-height: normal">
-                        Drs. Komaedi, M.Si
-                    </p>
 
-                    <p style="color: #063A69; font-size: 16px; font-weight: 700">
-                        Sekertaris Direktur
-                    </p>
+                    <div class="text-center pt-2">
+                        <div class="pb-2" style="font-weight: bold; font-size: 24px; line-height: normal;">
+                            Drs. Komaedi, M.Si
+                        </div>
 
+                        <div style="color: #063A69; font-size: 16px; font-weight: 700">
+                            Sekertaris Direktur
+                        </div>
+                    </div>
                 </div>
 
             </div>
@@ -43,45 +48,49 @@
 
                 {{-- hardiyanto_rahman --}}
                 <div class="col-3 text-center">
-                    <img src="{{ asset('assets/homepage/images/sotk_hardiyanto_rahman.png') }}" width="251px" height="276px"
-                        alt="" srcset="">
+                    <img src="{{ asset('assets/homepage/images/sotk_hardiyanto_rahman.png') }}" width="251px"
+                        height="276px" alt="" srcset="">
 
-                    <p class="text-center pt-2" style="font-weight: bold; font-size: 24px; line-height: normal">
-                        Hardiyanto Rahman, S.IP, M.Si
-                    </p>
 
-                    <p style="color: #063A69; font-size: 16px; font-weight: 700">
-                        Kepala Unit Laboratorium Pemerintahan, Pangakalan Data dan Sistem Informasi
-                    </p>
+                    <div class="text-center pt-2">
+                        <div class="pb-2" style="font-weight: bold; font-size: 24px; line-height: normal;">
+                            Hardiyanto Rahman, S.IP, M.Si
+                        </div>
 
+                        <div style="color: #063A69; font-size: 16px; font-weight: 700">
+                            Kepala Unit Laboratorium Pemerintahan, Pangakalan Data dan Sistem Informasi
+                        </div>
+                    </div>
                 </div>
 
                 <div class="col-3 rounded text-center">
                     <img src="{{ asset('assets/homepage/images/Group-115.png') }}" width="251px" height="276px"
                         alt="" srcset="" class="round">
 
-                    <p class="text-center pt-2" style="font-weight: bold; font-size: 24px; line-height: normal">
-                        Fransina Makdalena Paulla Nusmesse, S.Sos, M.Si
-                    </p>
-
-                    <p style="color: #063A69; font-size: 16px; font-weight: 700">
-                        Kepala Unit Penjaminan Mutu
-                    </p>
-
+                        <div class="text-center pt-2">
+                            <div class="pb-2" style="font-weight: bold; font-size: 24px; line-height: normal;">
+                                Fransina Makdalena Paulla Nusmesse, S.Sos, M.Si
+                            </div>
+    
+                            <div style="color: #063A69; font-size: 16px; font-weight: 700">
+                                Kepala Unit Penjaminan Mutu
+                            </div>
+                        </div>
                 </div>
 
                 <div class="col-3 text-center">
                     <img src="{{ asset('assets/homepage/images/Group-115.png') }}" width="251px" height="276px"
                         alt="" srcset="">
 
-                    <p class="text-center pt-2" style="font-weight: bold; font-size: 24px; line-height: normal">
-                        Cut Laila Karmila, S.Sos, M.A
-                    </p>
-
-                    <p style="color: #063A69; font-size: 16px; font-weight: 700">
-                        Kepala Unit Perpustakaan
-                    </p>
-
+                        <div class="text-center pt-2">
+                            <div class="pb-2" style="font-weight: bold; font-size: 24px; line-height: normal;">
+                                Cut Laila Karmila, S.Sos, M.A
+                            </div>
+    
+                            <div style="color: #063A69; font-size: 16px; font-weight: 700">
+                                Kepala Unit Perpustakaan
+                            </div>
+                        </div>
                 </div>
             </div>
 
@@ -95,14 +104,15 @@
                     <img src="{{ asset('assets/homepage/images/Group-115.png') }}" width="251px" height="276px"
                         alt="" srcset="">
 
-                    <p class="text-center pt-2" style="font-weight: bold; font-size: 24px; line-height: normal">
-                        Muhammad Hanif Nashrullah, S.STP
-                    </p>
-
-                    <p style="color: #063A69; font-size: 16px; font-weight: 700">
-                        Kepala Unit Penjaminan Mutu
-                    </p>
-
+                        <div class="text-center pt-2">
+                            <div class="pb-2" style="font-weight: bold; font-size: 24px; line-height: normal;">
+                                Muhammad Hanif Nashrullah, S.STP
+                            </div>
+    
+                            <div style="color: #063A69; font-size: 16px; font-weight: 700">
+                                Kepala Unit Penjaminan Mutu
+                            </div>
+                        </div>
                 </div>
 
                 <div class="col-3 text-center">
