0729f73aecab3b92b43bfbb0d2693182338b5e24
IndraMaulanaPratama <maulana.indra808@gmail.com>
2024-01-30 10:04:25 +0700

    Make link storeage to gambar

--- config/filesystems.php
+++ config/filesystems.php
@@ -73,6 +73,7 @@ return [
         public_path('storage') => storage_path('app/public'),
         public_path('foto_pegawai') => storage_path('app/public/foto_pegawai'),
         public_path('tanda_tangan') => storage_path('app/public/tanda_tangan'),
+        public_path('gambar') => storage_path('app/public/gambar'),
     ],
 
 ];
