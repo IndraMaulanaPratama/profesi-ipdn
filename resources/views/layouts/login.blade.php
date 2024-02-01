6b86b8f851f8948995ce95e11efec9cd70b27307
IndraMaulanaPratama <maulana.indra808@gmail.com>
2024-02-01 14:30:17 +0700

    FIXING BUG || style login page

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -5,12 +5,12 @@
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-    <title>{{ $title ?? 'Perpustakaan IPDN' }}</title>
+    <title>{{ $title ?? 'Profesi Kepamongprajaan IPDN' }}</title>
     <meta content="" name="description">
     <meta content="" name="keywords">
 
     <!-- Favicons -->
-    <link href="./assets/admin/img/favicon.png" rel="icon">
+    <link href="./assets/admin/img/favicon.ico" rel="icon">
     <link href="./assets/admin/img/apple-touch-icon.png" rel="apple-touch-icon">
 
     <!-- Google Fonts -->
@@ -20,16 +20,15 @@
         rel="stylesheet">
 
     <!-- Vendor CSS Files -->
-    <link href="./assets/admin/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="./assets/admin/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-    <link href="./assets/admin/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-    <link href="./assets/admin/vendor/quill/quill.snow.css" rel="stylesheet">
-    <link href="./assets/admin/vendor/quill/quill.bubble.css" rel="stylesheet">
-    <link href="./assets/admin/vendor/remixicon/remixicon.css" rel="stylesheet">
-    <link href="./assets/admin/vendor/simple-datatables/style.css" rel="stylesheet">
+    <link href="{{ asset('assets/admin/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/admin/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/admin/vendor/quill/quill.snow.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/admin/vendor/quill/quill.bubble.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/admin/vendor/remixicon/remixicon.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/admin/vendor/simple-datatables/style.css') }}" rel="stylesheet">
 
     <!-- Template Main CSS File -->
-    {{-- <link href="./assets/admin/css/style.css" rel="stylesheet"> --}}
+    <link href="{{ asset('assets/admin/css/style.css') }}" rel="stylesheet">
 
     <!-- =======================================================
   * Template Name: NiceAdmin
@@ -52,18 +51,22 @@
 
     <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
             class="bi bi-arrow-up-short"></i></a>
+
     <!-- Vendor JS Files -->
-    <script src="./assets/admin/vendor/apexcharts/apexcharts.min.js"></script>
+    <script src="{{ asset('assets/admin/vendor/tinymce/tinymce.min.js') }}"></script>
+    <script src="{{ asset('assets/admin/vendor/quill/quill.min.js') }}"></script>
+    <script src="{{ asset('assets/admin/vendor/simple-datatables/simple-datatables.js') }}"></script>
+    <script src="{{ asset('assets/admin/vendor/apexcharts/apexcharts.min.js') }}"></script>
+    <script src="{{ asset('assets/admin/vendor/chart.js/chart.umd.js') }}"></script>
+    <script src="{{ asset('assets/admin/vendor/echarts/echarts.min.js') }}"></script>
+    <script src="{{ asset('assets/admin/vendor/php-email-form/validate.js') }}"></script>
+
+    {{--
     <script src="./assets/admin/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="./assets/admin/vendor/chart.js/chart.umd.js"></script>
-    <script src="./assets/admin/vendor/echarts/echarts.min.js"></script>
-    <script src="./assets/admin/vendor/quill/quill.min.js"></script>
-    <script src="./assets/admin/vendor/simple-datatables/simple-datatables.js"></script>
-    <script src="./assets/admin/vendor/tinymce/tinymce.min.js"></script>
-    <script src="./assets/admin/vendor/php-email-form/validate.js"></script>
+    --}}
 
     <!-- Template Main JS File -->
-    {{-- <script src="./assets/admin/js/main.js"></script> --}}
+    <script src="{{ asset('assets/admin/js/main.js') }}"></script>
 
 </body>
 
