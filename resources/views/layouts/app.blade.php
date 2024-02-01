5a905310fbd0f70e9a975d9848287105b45d897f
IndraMaulanaPratama <maulana.indra808@gmail.com>
2024-02-01 15:22:28 +0700

    FIXING BUG || Style and script on login and app page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,15 +5,34 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
 
+    <!-- Favicons -->
     <link href="{{ asset('assets/homepage/images/favicon.ico') }}" rel="icon" />
     <link href="{{ asset('assets/homepage/images/apple-touch-icon.png') }}" rel="apple-touch-icon" />
 
     <title>{{ $title ?? 'Page Title' }}</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.gstatic.com" rel="preconnect">
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+        rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="{{ asset('assets/admin/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    {{-- <link href="{{ asset('assets/admin/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet"> --}}
+    {{-- <link href="{{ asset('assets/admin/vendor/quill/quill.snow.css') }}" rel="stylesheet"> --}}
+    {{-- <link href="{{ asset('assets/admin/vendor/quill/quill.bubble.css') }}" rel="stylesheet"> --}}
+    {{-- <link href="{{ asset('assets/admin/vendor/remixicon/remixicon.css') }}" rel="stylesheet"> --}}
+    {{-- <link href="{{ asset('assets/admin/vendor/simple-datatables/style.css') }}" rel="stylesheet"> --}}
+
+    <!-- Template Main CSS File -->
+    {{-- <link href="{{ asset('assets/admin/css/style.css') }}" rel="stylesheet"> --}}
 
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" />
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" /> --}}
+    {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script> --}}
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
@@ -25,35 +44,6 @@
     {{-- Navbar --}}
     <x-home.templates.navbar />
 
-    {{-- <nav class="navbar navbar-expand-lg bg-light">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false"
-                aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled">Disabled</a>
-                    </li>
-                </ul>
-                <form class="d-flex" role="search">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
-            </div>
-        </div>
-    </nav> --}}
-
-
     {{-- Content --}}
     <div class="container-fluid">
         {{ $slot }}
@@ -62,8 +52,11 @@
     {{-- Footer --}}
     <x-home.templates.footer />
 
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"></script>
+
+    <!-- Template Main JS File -->
+    {{-- <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script> --}}
+    {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"></script> --}}
+    <script src="{{ asset('assets/admin/js/main.js') }}"></script>
 
 </body>
 
--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -6,29 +6,29 @@
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
     <title>{{ $title ?? 'Profesi Kepamongprajaan IPDN' }}</title>
-    <meta content="" name="description">
-    <meta content="" name="keywords">
+    <meta content="" name="description" />
+    <meta content="" name="keywords" />
 
     <!-- Favicons -->
-    <link href="./assets/admin/img/favicon.ico" rel="icon">
-    <link href="./assets/admin/img/apple-touch-icon.png" rel="apple-touch-icon">
+    <link href="{{ asset('assets/homepage/images/favicon.ico') }}" rel="icon" />
+    <link href="{{ asset('assets/homepage/images/apple-touch-icon.png') }}" rel="apple-touch-icon" />
 
     <!-- Google Fonts -->
-    <link href="https://fonts.gstatic.com" rel="preconnect">
+    <link href="https://fonts.gstatic.com" rel="preconnect" />
     <link
         href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
-        rel="stylesheet">
+        rel="stylesheet" />
 
     <!-- Vendor CSS Files -->
-    <link href="{{ asset('assets/admin/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/admin/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/admin/vendor/quill/quill.snow.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/admin/vendor/quill/quill.bubble.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/admin/vendor/remixicon/remixicon.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/admin/vendor/simple-datatables/style.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/admin/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" />
+    {{-- <link href="{{ asset('assets/admin/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/admin/vendor/quill/quill.snow.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/admin/vendor/quill/quill.bubble.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/admin/vendor/remixicon/remixicon.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/admin/vendor/simple-datatables/style.css') }}" rel="stylesheet" /> --}}
 
     <!-- Template Main CSS File -->
-    <link href="{{ asset('assets/admin/css/style.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/admin/css/style.css') }}" rel="stylesheet" />
 
     <!-- =======================================================
   * Template Name: NiceAdmin
@@ -53,13 +53,13 @@
             class="bi bi-arrow-up-short"></i></a>
 
     <!-- Vendor JS Files -->
-    <script src="{{ asset('assets/admin/vendor/tinymce/tinymce.min.js') }}"></script>
+    {{-- <script src="{{ asset('assets/admin/vendor/tinymce/tinymce.min.js') }}"></script>
     <script src="{{ asset('assets/admin/vendor/quill/quill.min.js') }}"></script>
     <script src="{{ asset('assets/admin/vendor/simple-datatables/simple-datatables.js') }}"></script>
     <script src="{{ asset('assets/admin/vendor/apexcharts/apexcharts.min.js') }}"></script>
     <script src="{{ asset('assets/admin/vendor/chart.js/chart.umd.js') }}"></script>
     <script src="{{ asset('assets/admin/vendor/echarts/echarts.min.js') }}"></script>
-    <script src="{{ asset('assets/admin/vendor/php-email-form/validate.js') }}"></script>
+    <script src="{{ asset('assets/admin/vendor/php-email-form/validate.js') }}"></script> --}}
 
     {{--
     <script src="./assets/admin/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
