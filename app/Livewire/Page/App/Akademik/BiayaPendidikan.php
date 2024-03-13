e382876ce0a4e0b2478ae4553447295b7a17cead
IndraMaulanaPratama <maulana.indra808@gmail.com>
2024-03-13 11:46:59 +0700

    ADD NEW || Biaya pendidikan page

---
+++ app/Livewire/Page/App/Akademik/BiayaPendidikan.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Livewire\Page\App\Akademik;
+
+use Livewire\Attributes\Title;
+use Livewire\Component;
+
+
+#[Title('Biaya Pendidikan - Profesi Kepamongprajaan IPDN')]
+
+class BiayaPendidikan extends Component
+{
+    public function render()
+    {
+        return view('livewire.page.app.akademik.biaya-pendidikan');
+    }
+}
---
+++ resources/views/livewire/page/app/akademik/biaya-pendidikan.blade.php
@@ -0,0 +1,12 @@
+{{-- Do your work, then step back. --}}
+
+
+<div>
+    {{-- Header --}}
+    @livewire('App.Akademik.BiayaPendidikan.Header', [], key('header'))
+
+
+    {{-- Content --}}
+    @livewire('App.Akademik.BiayaPendidikan.Content', [], key('content'))
+
+</div>
