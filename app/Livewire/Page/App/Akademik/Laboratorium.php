8715ae72b149671b190ce6454f925c705c974b89
IndraMaulanaPratama <maulana.indra808@gmail.com>
2024-03-15 16:34:18 +0700

    ADD NEW || Laboratorium page on app

---
+++ app/Livewire/Page/App/Akademik/Laboratorium.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Livewire\Page\App\Akademik;
+
+use Livewire\Attributes\Title;
+use Livewire\Component;
+
+#[Title('Laboratorium - Profesi Kepamongprajaan IPDN')]
+
+class Laboratorium extends Component
+{
+    public function render()
+    {
+        return view('livewire.page.app.akademik.laboratorium');
+    }
+}
---
+++ resources/views/livewire/page/app/akademik/laboratorium.blade.php
@@ -0,0 +1,11 @@
+{{-- The best athlete wants his opponent at his best. --}}
+
+<div>
+    {{-- Header --}}
+    @livewire('App.Akademik.Laboratorium.Header', [], key('header'))
+
+
+    {{-- Content --}}
+    @livewire('App.Akademik.Laboratorium.Content', [], key('content'))
+
+</div>
