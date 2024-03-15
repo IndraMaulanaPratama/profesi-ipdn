d12f9866c573ffcf36ab6e71d05e450029bc4ee2
IndraMaulanaPratama <maulana.indra808@gmail.com>
2024-03-15 09:51:29 +0700

    ADD NEW || Kalender akademik page

---
+++ resources/views/livewire/page/app/akademik/kalender-akademik.blade.php
@@ -0,0 +1,11 @@
+{{-- Stop trying to control. --}}
+
+<div>
+    {{-- Header --}}
+    @livewire('App.Akademik.KalenderAkademik.Header', [], key('header'))
+
+
+    {{-- Content --}}
+    @livewire('App.Akademik.KalenderAkademik.Content', [], key('content'))
+
+</div>
