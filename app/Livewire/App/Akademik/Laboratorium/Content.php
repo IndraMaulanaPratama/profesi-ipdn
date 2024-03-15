868d39614d51aabb2c089a2f8e738aff3609203e
IndraMaulanaPratama <maulana.indra808@gmail.com>
2024-03-15 16:34:42 +0700

    ADD NEW || Content for laboratorium page

---
+++ app/Livewire/App/Akademik/Laboratorium/Content.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\App\Akademik\Laboratorium;
+
+use Livewire\Component;
+
+class Content extends Component
+{
+    public function render()
+    {
+        return view('livewire.app.akademik.laboratorium.content');
+    }
+}
---
+++ resources/views/livewire/app/akademik/laboratorium/content.blade.php
@@ -0,0 +1,302 @@
+{{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
+
+
+<div>
+    {{-- Gambar Thumbnail --}}
+    <div class="row my-5 text-center">
+        <div class="col justify-content-center">
+            <img src="{{ asset('assets/homepage/images/thumbnail-laboratorium.png') }}"
+                alt="Laboratorium Profesi Kepamongprajaan" height="383px" width="681px" />
+        </div>
+    </div>
+
+    {{-- Tab --}}
+    <div class="row justify-content-center my-5">
+        <div class="col-10">
+
+            {{-- Navigasi --}}
+            <nav class="my-5">
+                <div class="nav nav-tabs" id="nav-tab" role="tablist">
+
+                    {{-- Visi & Misi --}}
+                    <button class="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-visi"
+                        type="button" role="tab" aria-controls="nav-home" aria-selected="true">
+                        Visi & Misi
+                    </button>
+
+                    {{-- Layanan --}}
+                    <button class="nav-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-layanan"
+                        type="button" role="tab" aria-controls="nav-profile" aria-selected="false">
+                        Layanan
+                    </button>
+
+                    {{-- Pelatihan Indeks --}}
+                    <button class="nav-link" id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#nav-pelatihan "
+                        type="button" role="tab" aria-controls="nav-contact" aria-selected="false">
+                        Pelatihan Indeks
+                    </button>
+
+                    {{-- <button class="nav-link" id="nav-disabled-tab" data-bs-toggle="tab" data-bs-target="#nav-disabled"
+                        type="button" role="tab" aria-controls="nav-disabled" aria-selected="false"
+                        disabled>
+                        Disabled
+                    </button> --}}
+                </div>
+            </nav>
+
+            {{-- Content --}}
+            <div class="tab-content" id="nav-tabContent">
+
+                {{-- Visi & Misi --}}
+                <div class="tab-pane fade show active" id="nav-visi" role="tabpanel" aria-labelledby="nav-home-tab"
+                    tabindex="0">
+
+                    {{-- Konten Visi Misi --}}
+                    <div class="row text-center" style="margin-bottom: 100px;">
+                        {{-- Visi Area --}}
+                        <div>
+                            {{-- Header Visi --}}
+                            <div class="my-4" style="font-size: 36px; color: #063A69; font-weight: bold;">
+                                VISI
+                            </div>
+
+                            {{-- Konten Visi --}}
+                            <div class="" style="font-size: 24px; padding:0px 300px;">
+                                &#x201F;
+                                Menjadikan Laboratorium sebagai pusat kegiatan praktikum, pengembangan keilmuan,
+                                penelitian
+                                dan
+                                inovasi di Bidang Kepamongprajaan
+                                &#x301E;
+                            </div>
+                        </div>
+
+                        {{-- Misi Area --}}
+                        <div>
+                            {{-- Header Misi --}}
+                            <div class="my-5" style="font-size: 36px; color: #063A69; font-weight: bold;">
+                                MISI
+                            </div>
+
+                            {{-- Konten Misi --}}
+                            <div class="row justify-content-center" style="font-size: 16px;">
+
+                                <div class="col-3">
+                                    <div class="row justify-content-between">
+                                        <div class="col-11">
+
+                                            Menyelenggarakan praktikum, pelatihan, diskusi dan kajian sesuai dengan
+                                            kebutuhan
+                                            mahasiswa
+                                            dan dosen.
+                                        </div>
+
+                                        <div class="col-auto" style="margin: 10px 0;">
+                                            <div class="rounded" style="border-left: 3px solid #eab940; height: 30px; ">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="col-3">
+                                    <div class="row justify-content-between">
+                                        <div class="col-11">
+
+                                            Melakukan penelitian yang berkualitas dan berbasis ilmu pengetahuan
+                                            teknologi
+                                            dalam
+                                            bidang
+                                            kepamongprajaan.
+
+                                        </div>
+
+                                        <div class="col-auto" style="margin: 10px 0;">
+                                            <div class="rounded" style="border-left: 3px solid #eab940; height: 30px; ">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="col-3">
+                                    <div class="row justify-content-between">
+                                        <div class="col-11">
+
+                                            Melakukan kunjungan edukatif sebagai sarana peningkatan kompetensi dan
+                                            kemampuan
+                                            kewilayahan
+                                            dalam lingkup kepamongprajaan.
+                                        </div>
+
+                                        <div class="col-auto" style="margin: 10px 0;">
+                                            <div class="rounded" style="border-left: 3px solid #eab940; height: 30px; ">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="col-3">
+                                    <div class="row justify-content-between">
+                                        <div class="col-11">
+
+                                            Melakukan kerjasama dalam rangka pengembangan keilmuan bidang
+                                            kepamongprajaan.
+                                        </div>
+
+                                        <div class="col-auto" style="margin: 10px 0;">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    {{-- Konten Maksud dan Tujuan --}}
+                    <div class="row justify-content-start shadow" style="background-color: #EFEFEF; border-radius: 4px;">
+                        {{-- Maksud Area --}}
+                        <div class="text-center mb-5">
+                            {{-- Header Maksud --}}
+                            <div class="my-4" style="font-size: 36px; color: #063A69; font-weight: bold;">
+                                MAKSUD
+                            </div>
+
+                            {{-- Konten Maksud --}}
+                            <div class="" style="font-size: 24px; padding:0px 300px;">
+                                &#x201F;
+                                Meningkatkan kompetensi Mahasiswa dan pengembangan keilmuan di bidang
+                                Kepamongprajaan
+                                &#x301E;
+                            </div>
+                        </div>
+
+                        {{-- Tujuan Area --}}
+                        <div class="row">
+                            {{-- Header Tujuan --}}
+                            <div class="my-4 text-center" style="font-size: 36px; color: #063A69; font-weight: bold;">
+                                TUJUAN
+                            </div>
+
+                            {{-- Konten Tujuan --}}
+                            <div class="" style="font-size: 20px; padding:0px 300px; text-align: justify;">
+                                <ol type="1">
+                                    <li class="mb-4">
+                                        Membekali kompetensi Mahasiswa dalam memahami dan mengaplikasikan Indeks
+                                        desa
+                                        membangun, indeks pelayanan publik, indeks evaluasi perkembangan desa dan
+                                        kelurahan, dan indeks pelayanan publik.
+                                    </li>
+
+                                    <li class="mb-4">
+                                        Membekali kompetensi mahasiswa dalam bentuk diskusi ilmiah, dan kajian
+                                        bersama
+                                        para dosen.
+                                    </li>
+
+                                    <li class="mb-4">
+                                        Memfasilitasi penyelenggaraan penelitian bidang kepamongprajaan bagi
+                                        mahasiswa
+                                        dan dosen.
+                                    </li>
+
+                                    <li class="mb-4">
+                                        Memfasilitasi mahasiswa dalam pelaksanaan studi perspektif penyelenggaraan
+                                        pemerintahan pada unit kerja lainnya
+                                    </li>
+
+                                    <li class="mb-4">
+                                        Menjalin dan Mengembangkan kemitraan dengan kementerian/ lembaga dan
+                                        pemerintah
+                                        daerah, perguran tinggi, lembaga swadaya, masyarakat dan dunia usaha dalam
+                                        rangka pengembangan keilmuan.
+                                    </li>
+                                </ol>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                {{-- Layanan --}}
+                <div class="tab-pane fade" id="nav-layanan" role="tabpanel" aria-labelledby="nav-profile-tab"
+                    tabindex="0">
+
+                    {{-- Konten Layanan --}}
+                    <div class="row justify-content-start">
+
+
+                        {{-- Layanan Area --}}
+                        <div class="row">
+                            {{-- Header Layanan --}}
+                            <div class="my-4 text-center" style="font-size: 36px; color: #063A69; font-weight: bold;">
+                                LAYANAN LABORATORIUM
+                            </div>
+
+                            {{-- Konten Layanan --}}
+                            <div class="" style="padding:0px 300px; text-align: justify;">
+
+                                {{-- Praktikum Perkuliahan --}}
+                                <div class="">
+                                    {{-- Header --}}
+                                    <div class="">
+                                        <p
+                                            style="background-color: #063A69; color: #FFFFFF; font-size:18px; text-align: justify; padding: 10px 20px; border-radius: 4px;">
+                                            {{ Str::upper('1. praktikum perkuliahan') }}
+                                        </p>
+                                    </div>
+                                </div>
+
+                                {{-- Pelatihan Index --}}
+                                <div class="">
+                                    {{-- Header --}}
+                                    <div class="">
+                                        <p
+                                            style="background-color: #063A69; color: #FFFFFF; font-size:18px; text-align: justify; padding: 10px 20px; border-radius: 4px;">
+                                            {{ Str::upper('2. Pelatihan Indeks bagi mahasiswa') }}
+                                        </p>
+                                    </div>
+                                </div>
+
+                                {{-- Diskusi & Kajian --}}
+                                <div class="">
+                                    {{-- Header --}}
+                                    <div class="">
+                                        <p
+                                            style="background-color: #063A69; color: #FFFFFF; font-size:18px; text-align: justify; padding: 10px 20px; border-radius: 4px;">
+                                            {{ Str::upper('3. Diskusi & Kajian bagi Dosen dan Mahasiswa Profesi Kepamongprajaan') }}
+                                        </p>
+                                    </div>
+                                </div>
+
+
+                                {{-- Laboratorium Dinamis --}}
+                                <div class="">
+                                    {{-- Header --}}
+                                    <div class="">
+                                        <p
+                                            style="background-color: #063A69; color: #FFFFFF; font-size:18px; text-align: justify; padding: 10px 20px; border-radius: 4px;">
+                                            {{ Str::upper('4. Laboratorium Dinamis') }}
+                                        </p>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                {{-- Pelatihan Index --}}
+                <div class="tab-pane fade" id="nav-pelatihan" role="tabpanel" aria-labelledby="nav-contact-tab"
+                    tabindex="0">
+                    Latihlah aku tuk menjadi pendamping hidupmu 🤎
+                </div>
+
+                {{-- Contoh Kontent --}}
+                {{-- <div class="tab-pane fade" id="nav-disabled" role="tabpanel" aria-labelledby="nav-disabled-tab"
+                    tabindex="0">
+                    disabled
+                </div> --}}
+            </div>
+
+        </div>
+
+
+    </div>
+</div>
