25ec3c38919446ec541a98d5b3fb653f13f24b1e
IndraMaulanaPratama <maulana.indra808@gmail.com>
2024-03-15 16:34:56 +0700

    ADD NEW || Header for laboratorium page

---
+++ app/Livewire/App/Akademik/Laboratorium/Header.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\App\Akademik\Laboratorium;
+
+use Livewire\Component;
+
+class Header extends Component
+{
+    public function render()
+    {
+        return view('livewire.app.akademik.laboratorium.header');
+    }
+}
