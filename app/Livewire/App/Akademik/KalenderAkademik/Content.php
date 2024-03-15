454e887363be223740f079134f441c728674faaa
IndraMaulanaPratama <maulana.indra808@gmail.com>
2024-03-15 11:24:10 +0700

    ADD NEW || Content for kalender akademik app

---
+++ app/Livewire/App/Akademik/KalenderAkademik/Content.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\App\Akademik\KalenderAkademik;
+
+use Livewire\Component;
+
+class Content extends Component
+{
+    public function render()
+    {
+        return view('livewire.app.akademik.kalender-akademik.content');
+    }
+}
---
+++ resources/views/livewire/app/akademik/kalender-akademik/content.blade.php
@@ -0,0 +1,140 @@
+{{-- Close your eyes. Count to one. That is how long forever feels. --}}
+
+
+<div class="container">
+    {{-- Header Biaya Pendidikan --}}
+    <div class="text-center my-5" style="font-weight: bold; font-size: 24px">
+        Tahun Akademik 2024/2025 <br />
+        <center>
+            <div class="rounded" style="border: 3px solid #eab940; height: 2px; width:100px; background-color: #eab940;">
+        </center>
+    </div>
+
+    {{-- Table --}}
+    <table class="table table-bordered text-center my-5" style="font-size: 20px">
+        <thead class="table-primary">
+            <th>TAHUN</th>
+            <th>TANGGAL / BULAN</th>
+            <th>SEMESTER</th>
+            <th width="45%">KEGIATAN</th>
+        </thead>
+
+        <tbody>
+            {{-- tahun 2024 --}}
+            <tr>
+                <td rowspan="18">2025</td>
+
+                <td>2 Januari - 16 Februari</td>
+
+                <td rowspan="11"> I </td>
+
+                <td>Pembukaan Pendaftaran</td>
+            </tr>
+
+            <tr>
+                <td>19 - 23 Februari</td>
+                <td>
+                    Seleksi Administrasi, Tes Wawancara, Penempatan Asrama Mahasiswa dan Kelengkapan Seragam / Atribut
+                </td>
+            </tr>
+
+            <tr>
+                <td>26 - 27 Februari</td>
+                <td>Pekan Integrasi Mahasiswa, Pengenalan Kampus dan Pembinaan Tata Kehidupan Kampus</td>
+            </tr>
+
+            <tr>
+                <td>28 Februari - 1 Maret</td>
+                <td>Pembukaan PerkuliahanKuliah Umum dan Matrikulasi</td>
+            </tr>
+
+            <tr>
+                <td>4 Maret - 5 April</td>
+                <td>Perkuliahan Semester I</td>
+            </tr>
+
+            <tr>
+                <td>8 - 19 April</td>
+                <td>Libur Hari Raya Idul Fitri</td>
+            </tr>
+
+            <tr>
+                <td>22 April - 3 Mei</td>
+                <td>Lanjutan Perkuliahan Semester I</td>
+            </tr>
+
+            <tr>
+                <td>6 - 10 Mei</td>
+                <td>Ujian Tengah Semester I</td>
+            </tr>
+
+            <tr>
+                <td>13 Mei - 28 Juni</td>
+                <td>Lanjutan Perkuliahan Semester I</td>
+            </tr>
+
+            <tr>
+                <td>1 - 5 Juli</td>
+                <td>Ujian Akhir Semester I</td>
+            </tr>
+
+            <tr>
+                <td>8 - 12 Juli</td>
+                <td>
+                    Pembekalan Praktik Lapangan dan Bimbingan Penulisan Laporan Praktik
+                </td>
+            </tr>
+
+            <tr>
+                <td>15 Juli - 13 November</td>
+                <td rowspan="9">II</td>
+                <td>Praktik Lapangan Kepamongprajaan</td>
+            </tr>
+
+            <tr>
+                <td>14 - 15 November</td>
+                <td>Kembali ke Kampus</td>
+            </tr>
+
+            <tr>
+                <td>18 - 22 November</td>
+                <td>
+                    Lanjutan Bimbingan dan Penyelesaisan Penulisan Laporan Praktik
+                </td>
+            </tr>
+
+            <tr>
+                <td>25 - 29 November</td>
+                <td>Pendaftaran Ujian Laporan Praktik</td>
+            </tr>
+
+            <tr>
+                <td>2 - 6 Desember</td>
+                <td>Ujian Laporan Praktik</td>
+            </tr>
+
+            <tr>
+                <td>9 - 13 Desember</td>
+                <td>Perbaikan dan Pengumpulan Laporan Praktik</td>
+            </tr>
+
+            <tr>
+                <td>16 - 31 Desember</td>
+                <td>Libur Hari Raya Natal</td>
+            </tr>
+
+            {{-- tahun 2025 --}}
+            <tr>
+                <td rowspan="2">2025</td>
+                <td>1 - 3 Januari</td>
+                <td>Libur Tahun Baru</td>
+            </tr>
+
+            <tr>
+                <td>30/31 Januari</td>
+                <td>Yudisium/Wisuda</td>
+            </tr>
+        </tbody>
+    </table>
+
+</div>
