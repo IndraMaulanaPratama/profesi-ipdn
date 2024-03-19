932314576c1ff698b5fb5bcbf42f26a34ca65989
IndraMaulanaPratama <maulana.indra808@gmail.com>
2024-03-19 09:03:30 +0700

    ADD NEW || Header for fasilitas page

---
+++ app/Livewire/App/Profile/Fasilitas/Fasilitas/Header.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\App\Profile\Fasilitas\Fasilitas;
+
+use Livewire\Component;
+
+class Header extends Component
+{
+    public function render()
+    {
+        return view('livewire.app.profile.fasilitas.fasilitas.header');
+    }
+}
