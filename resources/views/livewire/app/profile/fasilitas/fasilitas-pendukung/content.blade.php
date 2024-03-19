04159aa8ef2cfad5136a5a90186160057064e014
IndraMaulanaPratama <maulana.indra808@gmail.com>
2024-03-19 08:46:41 +0700

    UPDATE || Content fasilitas pendukung page

--- resources/views/livewire/app/profile/fasilitas/fasilitas-pendukung/content.blade.php
+++ resources/views/livewire/app/profile/fasilitas/fasilitas-pendukung/content.blade.php
@@ -1,3 +1,58 @@
-<div>
-    {{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day. --}}
+{{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day. --}}
+
+
+<div class="container my-5">
+
+    <div class="row justify-content-center text-center my-3 ">
+
+        <div class="col-6" style="">
+            <img src="{{ asset('assets/homepage/images/fasilitas-pendukung/fasilitas-pendukung-1.png') }}" alt="fasilitas-pendukung-1"
+                style="width: 550px; height: 373px;" />
+        </div>
+
+        <div class="col-6" style="">
+            <img src="{{ asset('assets/homepage/images/fasilitas-pendukung/fasilitas-pendukung-2.png') }}" alt="fasilitas-pendukung-2"
+                style="width: 550px; height: 373px;" />
+        </div>
+    </div>
+
+
+    <div class="row justify-content-center text-center my-3">
+
+        <div class="col-6" style="">
+            <img src="{{ asset('assets/homepage/images/fasilitas-pendukung/fasilitas-pendukung-3.png') }}" alt="fasilitas-pendukung-3"
+                style="width: 550px; height: 373px;" />
+        </div>
+
+        <div class="col-6" style="">
+            <img src="{{ asset('assets/homepage/images/fasilitas-pendukung/fasilitas-pendukung-4.png') }}" alt="fasilitas-pendukung-4"
+                style="width: 550px; height: 373px;" />
+        </div>
+    </div>
+
+    <div class="row justify-content-center text-center my-3">
+
+        <div class="col-6" style="">
+            <img src="{{ asset('assets/homepage/images/fasilitas-pendukung/fasilitas-pendukung-5.png') }}" alt="fasilitas-pendukung-5"
+                style="width: 550px; height: 373px;" />
+        </div>
+
+        <div class="col-6" style="">
+            <img src="{{ asset('assets/homepage/images/fasilitas-pendukung/fasilitas-pendukung-6.png') }}" alt="fasilitas-pendukung-6"
+                style="width: 550px; height: 373px;" />
+        </div>
+    </div>
+
+    <div class="row justify-content-center text-center my-3">
+
+        <div class="col-6" style="">
+            <img src="{{ asset('assets/homepage/images/fasilitas-pendukung/fasilitas-pendukung-7.png') }}" alt="fasilitas-pendukung-7"
+                style="width: 550px; height: 373px;" />
+        </div>
+
+        <div class="col-6" style="">
+            <img src="{{ asset('assets/homepage/images/fasilitas-pendukung/fasilitas-pendukung-8.png') }}" alt="fasilitas-pendukung-8"
+                style="width: 550px; height: 373px;" />
+        </div>
+    </div>
 </div>
