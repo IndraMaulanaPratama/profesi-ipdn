b2169fd68d0ca2968566b02e02ee03dcb448d804
IndraMaulanaPratama <maulana.indra808@gmail.com>
2024-03-19 09:04:33 +0700

    UPDATE || Add component header and content on fasilitas page

--- resources/views/livewire/page/app/profile/fasilitas/fasilitas.blade.php
+++ resources/views/livewire/page/app/profile/fasilitas/fasilitas.blade.php
@@ -1,3 +1,11 @@
+{{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
+
 <div>
-    {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
+    {{-- Header --}}
+    @livewire('App.Profile.Fasilitas.Fasilitas.Header', [], key('header'))
+
+
+    {{-- Content --}}
+    @livewire('App.Profile.Fasilitas.Fasilitas.Content', [], key('content'))
+
 </div>
