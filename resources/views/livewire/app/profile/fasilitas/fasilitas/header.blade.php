33b4035e91ea5929610c6b5a192547f180e028a7
IndraMaulanaPratama <maulana.indra808@gmail.com>
2024-03-19 09:05:00 +0700

    UPDATE || Content on header fasilitas page

--- resources/views/livewire/app/profile/fasilitas/fasilitas/header.blade.php
+++ resources/views/livewire/app/profile/fasilitas/fasilitas/header.blade.php
@@ -1,3 +1,50 @@
+{{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day. --}}
+
 <div>
-    {{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day. --}}
+
+    {{-- Breadcrumb --}}
+    <div class="container mb-3" style="font-size: 12px;">
+        <a href="{{ route('/') }}" class="link" style="">
+            Beranda
+        </a>
+
+        &nbsp; &#62; &nbsp;
+
+        <a href="#">
+            Profile
+        </a>
+
+        &nbsp; &#62; &nbsp;
+
+        <a href="#">
+            Fasilitas
+        </a>
+
+        &nbsp; &#62; &nbsp;
+
+        <a href="{{ route('profile-fasilitas.fasilitas') }}">
+            Fasilitas
+        </a>
+    </div>
+
+    {{-- Jumbotron --}}
+    <div class="row">
+        <div class="col-12">
+            <!-- Jumbotron -->
+            <div class="text-center bg-image"
+                style="background-image: url('https://www.wallpapers13.com/wp-content/uploads/2016/01/Landscape-beautiful-nature-green-tropical-waterfall-rocks-covered-with-moss-green-turquoise-green-water-Desktop-HD-Wallpaper-for-Mobile-phones-Tablet-and-PC-3840x2400-1920x1440.jpg');height: auto;">
+
+                <div class="mask" style="background-color: rgba(0, 0, 0, 0.6);">
+                    <div class="d-flex justify-content-center align-items-center" style=" height: 200px">
+                        <div style="color: #ffda79;">
+                            <h1 class=""><b>FASILITAS</b></h1>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <!-- Jumbotron -->
+        </div>
+    </div>
+
 </div>
