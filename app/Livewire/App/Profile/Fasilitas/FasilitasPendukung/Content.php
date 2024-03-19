a7a7332a177bba1e40967b6fa960553ab94cfde8
IndraMaulanaPratama <maulana.indra808@gmail.com>
2024-03-19 08:28:43 +0700

    ADD NEW || Content on Fasilitas Pendukung

---
+++ app/Livewire/App/Profile/Fasilitas/FasilitasPendukung/Content.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\App\Profile\Fasilitas\FasilitasPendukung;
+
+use Livewire\Component;
+
+class Content extends Component
+{
+    public function render()
+    {
+        return view('livewire.app.profile.fasilitas.fasilitas-pendukung.content');
+    }
+}
