bb5f444fceaef75c18d07b20ad09c29e790b9720
IndraMaulanaPratama <maulana.indra808@gmail.com>
2024-03-19 09:03:58 +0700

    FIXING BUG || Hyperlink on header fasilitas pendukung page

--- resources/views/livewire/app/profile/fasilitas/fasilitas-pendukung/header.blade.php
+++ resources/views/livewire/app/profile/fasilitas/fasilitas-pendukung/header.blade.php
@@ -24,7 +24,7 @@
 
         &nbsp; &#62; &nbsp;
 
-        <a href="{{ route('profile-sotk') }}">
+        <a href="{{ route('profile-fasilitas.fasilitasPendukung') }}">
             Fasilitas Pendukung
         </a>
     </div>
