0891805fa448e935e94f74dd2929c9dc650e1866
IndraMaulanaPratama <maulana.indra808@gmail.com>
2024-03-19 09:27:52 +0700

    UPDATE || Content for fasilitas page

---
+++ app/Livewire/App/Profile/Fasilitas/Fasilitas/Content.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\App\Profile\Fasilitas\Fasilitas;
+
+use Livewire\Component;
+
+class Content extends Component
+{
+    public function render()
+    {
+        return view('livewire.app.profile.fasilitas.fasilitas.content');
+    }
+}
---
+++ resources/views/livewire/app/profile/fasilitas/fasilitas/content.blade.php
@@ -0,0 +1,91 @@
+{{-- The best athlete wants his opponent at his best. --}}
+
+<div class="container my-5 text-center">
+
+    {{-- Sekretariat --}}
+    <div class="mb-5">
+
+        {{-- Header Sekretariat --}}
+        <div class="mb-4" style="font-size: 37px; color: #063A69; font-weight: bold;">
+            {{ Str::upper('sekretariat') }}
+
+            <center>
+                <div class="" style="margin: 0px 0; width: 70px;">
+                    <div class="rounded" style="border: 2px solid #eab940; width: 50px;">
+                    </div>
+                </div>
+            </center>
+        </div>
+
+
+        {{-- Content Sekretariat --}}
+        <img src="{{ asset('assets/homepage/images/fasilitas/fasilitas-1.png') }}" alt="fasilitas-1"
+            style="widows: 577px; height: 341px" />
+    </div>
+
+
+    {{-- Laboratorium --}}
+    <div class="my-5">
+
+        {{-- Header Laboratorium --}}
+        <div class="mb-4" style="font-size: 37px; color: #063A69; font-weight: bold;">
+            {{ Str::upper('Laboratorium') }}
+
+            <center>
+                <div class="" style="margin: 0px 0; width: 70px;">
+                    <div class="rounded" style="border: 2px solid #eab940; width: 50px;">
+                    </div>
+                </div>
+            </center>
+        </div>
+
+        {{-- Content Laboratorium --}}
+        <img src="{{ asset('assets/homepage/images/fasilitas/fasilitas-2.png') }}" alt="fasilitas-2"
+            style="widows: 577px; height: 341px" />
+
+    </div>
+
+
+    {{-- Laboratorium --}}
+    <div class="mt-5">
+
+        {{-- Header Perpustakaan --}}
+        <div class="my-5" style="font-size: 37px; color: #063A69; font-weight: bold;">
+            {{ Str::upper('Perpustakaan') }}
+
+            <center>
+                <div class="" style="margin: 0px 0; width: 70px;">
+                    <div class="rounded" style="border: 2px solid #eab940; width: 50px;">
+                    </div>
+                </div>
+            </center>
+        </div>
+
+        {{-- Content Laboratorium --}}
+        <div class="row justify-content-center">
+            <div class="col-10">
+
+                <div class="row justify-content-center">
+                    <div class="col-3">
+                        <img src="{{ asset('assets/homepage/images/fasilitas/fasilitas-3.png') }}" alt="fasilitas-3"
+                            style="width: 243px; height: 243xpx" style="widows: 577px; height: 341px" />
+                    </div>
+
+                    <div class="col-3">
+                        <img src="{{ asset('assets/homepage/images/fasilitas/fasilitas-4.png') }}" alt="fasilitas-4"
+                            style="width: 243px; height: 243xpx" style="widows: 577px; height: 341px" />
+                    </div>
+
+                    <div class="col-3">
+                        <img src="{{ asset('assets/homepage/images/fasilitas/fasilitas-5.png') }}" alt="fasilitas-5"
+                            style="width: 243px; height: 243xpx" style="widows: 577px; height: 341px" />
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+
+
+</div>
