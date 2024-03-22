a3f6f6b84e654f353a171f2f5c39452aa234f6c7
IndraMaulanaPratama <maulana.indra808@gmail.com>
2024-03-22 11:01:20 +0700

    ADD NEW || Content component for kegiatan kemahasiswaan page

---
+++ app/Livewire/App/Kemahasiswaan/Kegiatan/Content.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\App\Kemahasiswaan\Kegiatan;
+
+use Livewire\Component;
+
+class Content extends Component
+{
+    public function render()
+    {
+        return view('livewire.app.kemahasiswaan.kegiatan.content');
+    }
+}
---
+++ resources/views/livewire/app/kemahasiswaan/kegiatan/content.blade.php
@@ -0,0 +1,136 @@
+{{-- Success is as dangerous as failure. --}}
+
+
+<div class="container my-5">
+
+    {{-- Content 1 --}}
+    <div>
+
+        {{-- Header --}}
+        <div>
+            {{-- Title --}}
+            <div style="font-size: 24px; color: #063A69; font-weight: bold">
+                Pengenalan Kampus Mahasiswa Ak. XIII
+            </div>
+
+            {{-- Date Time --}}
+            <div>
+
+                {{-- Date --}}
+                <div style="font-size: 16px;">
+                    01 Maret 2024
+                </div>
+
+                {{-- Line --}}
+                <div class="rounded"
+                    style="border: 2px solid #eab940; height: 2px; width:70px; background-color: #eab940;">
+
+                </div>
+            </div>
+
+            {{-- Content --}}
+            <div class="row my-4">
+
+                <div class="col-4">
+                    <img src="{{ asset('assets/homepage/images/kemahasiswaa/kegiatan/1.png') }}" alt="kegiatan 1"
+                        style="width: 400px; height: 250px;" />
+                </div>
+                <div class="col-4">
+                    <img src="{{ asset('assets/homepage/images/kemahasiswaa/kegiatan/2.png') }}" alt="kegiatan 2"
+                        style="width: 400px; height: 250px;" />
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+    {{-- Content 2 --}}
+    <div>
+
+        {{-- Header --}}
+        <div>
+            {{-- Title --}}
+            <div style="font-size: 24px; color: #063A69; font-weight: bold">
+                Senam Pagi Bersama Mahasiswa Ak. XIII
+            </div>
+
+            {{-- Date Time --}}
+            <div>
+
+                {{-- Date --}}
+                <div style="font-size: 16px;">
+                    01 Maret 2024
+                </div>
+
+                {{-- Line --}}
+                <div class="rounded"
+                    style="border: 2px solid #eab940; height: 2px; width:70px; background-color: #eab940;">
+
+                </div>
+            </div>
+
+            {{-- Content --}}
+            <div class="row my-4">
+
+                <div class="col-4">
+                    <img src="{{ asset('assets/homepage/images/kemahasiswaa/kegiatan/3.png') }}" alt="kegiatan 1"
+                        style="width: 400px; height: 250px;" />
+                </div>
+                <div class="col-4">
+                    <img src="{{ asset('assets/homepage/images/kemahasiswaa/kegiatan/4.png') }}" alt="kegiatan 2"
+                        style="width: 400px; height: 250px;" />
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+    {{-- Content 3 --}}
+    <div>
+
+        {{-- Header --}}
+        <div>
+            {{-- Title --}}
+            <div style="font-size: 24px; color: #063A69; font-weight: bold">
+                Pembukaan Matrikulasi & Perkuliahan Mahasiswa Ak. XIII
+            </div>
+
+            {{-- Date Time --}}
+            <div>
+
+                {{-- Date --}}
+                <div style="font-size: 16px;">
+                    28 Februari 2024
+                </div>
+
+                {{-- Line --}}
+                <div class="rounded"
+                    style="border: 2px solid #eab940; height: 2px; width:70px; background-color: #eab940;">
+
+                </div>
+            </div>
+
+            {{-- Content --}}
+            <div class="row my-4">
+
+                <div class="col-4">
+                    <img src="{{ asset('assets/homepage/images/kemahasiswaa/kegiatan/5.png') }}" alt="kegiatan 1"
+                        style="width: 400px; height: 250px;" />
+                </div>
+                <div class="col-4">
+                    <img src="{{ asset('assets/homepage/images/kemahasiswaa/kegiatan/6.png') }}" alt="kegiatan 2"
+                        style="width: 400px; height: 250px;" />
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+
+</div>
