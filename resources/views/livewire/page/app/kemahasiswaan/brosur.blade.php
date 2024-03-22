e7023295b3a10fdc5684b0c3989f7f253a09650a
IndraMaulanaPratama <maulana.indra808@gmail.com>
2024-03-22 10:59:57 +0700

    ADD NEW || Brosur page

---
+++ resources/views/livewire/page/app/kemahasiswaan/brosur.blade.php
@@ -0,0 +1,12 @@
+{{-- Be like water. --}}
+
+<div>
+
+    {{-- Header --}}
+    @livewire('App.kemahasiswaan.brosur.header', [], key('header'))
+
+
+    {{-- Content --}}
+    @livewire('App.kemahasiswaan.brosur.content', [], key('content'))
+
+</div>
