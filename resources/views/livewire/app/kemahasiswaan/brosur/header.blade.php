5f5b75895e18103378bda25a5d46e2ed99b39796
IndraMaulanaPratama <maulana.indra808@gmail.com>
2024-03-22 11:00:39 +0700

    ADD NEW || Header component for brosure page

---
+++ resources/views/livewire/app/kemahasiswaan/brosur/header.blade.php
@@ -0,0 +1,45 @@
+{{-- Success is as dangerous as failure. --}}
+
+
+<div>
+
+    {{-- Breadcrumb --}}
+    <div class="container mb-3" style="font-size: 12px;">
+        <a href="{{ route('/') }}" class="link" style="">
+            Beranda
+        </a>
+
+        &nbsp; &#62; &nbsp;
+
+        <a href="#">
+            Kemahasiswaan
+        </a>
+
+        &nbsp; &#62; &nbsp;
+
+        <a href="{{ route('kemahasiswaan-brosur') }}">
+            Brosur
+        </a>
+    </div>
+
+    {{-- Jumbotron --}}
+    <div class="row">
+        <div class="col-12">
+            <!-- Jumbotron -->
+            <div class="text-center bg-image"
+                style="background-image: url('https://www.wallpapers13.com/wp-content/uploads/2016/01/Landscape-beautiful-nature-green-tropical-waterfall-rocks-covered-with-moss-green-turquoise-green-water-Desktop-HD-Wallpaper-for-Mobile-phones-Tablet-and-PC-3840x2400-1920x1440.jpg');height: auto;">
+
+                <div class="mask" style="background-color: rgba(0, 0, 0, 0.6);">
+                    <div class="d-flex justify-content-center align-items-center" style=" height: 200px">
+                        <div style="color: #ffda79;">
+                            <h1 class=""><b>BROSUR PPPKP</b></h1>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <!-- Jumbotron -->
+        </div>
+    </div>
+
+</div>
