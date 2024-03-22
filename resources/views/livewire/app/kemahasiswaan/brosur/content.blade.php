03a76227283f7f2ce45c4c0ace2fc6b956932ad0
IndraMaulanaPratama <maulana.indra808@gmail.com>
2024-03-22 11:01:08 +0700

    ADD NEW || Content component for brosure page

---
+++ resources/views/livewire/app/kemahasiswaan/brosur/content.blade.php
@@ -0,0 +1,15 @@
+{{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
+
+<div class="container">
+
+    <div class="row text-center my-5 g-4">
+        <div class="col-12">
+            <img src="{{asset('assets/homepage/images/kemahasiswaa/brosur/BROSUR 1.png')}}" alt="Brosur 1" style="width: 792px; height: 612px;" />
+        </div>
+
+        <div class="col-12">
+            <img src="{{asset('assets/homepage/images/kemahasiswaa/brosur/BROSUR 2.png')}}" alt="Brosur " style="width: 792px; height: 612px;" />
+        </div>
+
+    </div>
+</div>
