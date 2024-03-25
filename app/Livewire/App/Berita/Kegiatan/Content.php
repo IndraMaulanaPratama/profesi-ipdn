c6d0aac029c87c8fe615931c6d5290bbf10cee35
IndraMaulanaPratama <maulana.indra808@gmail.com>
2024-03-25 14:53:20 +0700

    ADD NEW || Element Content Kegiatan on Berita

---
+++ app/Livewire/App/Berita/Kegiatan/Content.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\App\Berita\Kegiatan;
+
+use Livewire\Component;
+
+class Content extends Component
+{
+    public function render()
+    {
+        return view('livewire.app.berita.kegiatan.content');
+    }
+}
---
+++ resources/views/livewire/app/berita/kegiatan/content.blade.php
@@ -0,0 +1,179 @@
+{{-- Care about people's approval and you will be their prisoner. --}}
+
+<div class="container my-3">
+    <div class="row ">
+
+        {{-- Berita --}}
+        <div class="col-8">
+
+            {{-- Kegiatan 1 --}}
+            <div class="row my-4">
+                <div class="col-12">
+                    <div class="card shadow" style="">
+                        <img src="{{ asset('assets/homepage/images/berita/kegiatan/1.png') }}" class="card-img-top"
+                            alt="...">
+                        <div class="card-body">
+                            <div class="pb-2" style="color: #E3A100; font-size: 12px">29 Februari 2024</div>
+
+                            <h5 class="card-title">
+                                PEMBUKAAN MATRIKULASI DAN PERKULIAHAN PROGRAM PROFESI KEPAMONGPRAJAAN AK. XIII
+                            </h5>
+
+                            <p class="card-text" style="font-size: 16px; text-align: justify;">
+                                Jakarta - Rektor IPDN, Prof. Dr. Drs. H. Hadi Prabowo., M.M resmi membuka kegiatan
+                                matrikulasi dan perkuliahan Program Studi Profesi Kepamongprajaan angkatan XIII tahun
+                                akademik 2024/2025. Terdapat 36 mahasiswa dari berbagai daerah seperti Garut, Kuningan,
+                                dll.
+                            </p>
+                            <a href="#" class="btn btn-primary">Baca Selengkapnya</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {{-- Kegiatan 2 --}}
+            <div class="row my-4">
+                <div class="col-12">
+                    <div class="card shadow" style="">
+                        <img src="{{ asset('assets/homepage/images/berita/kegiatan/2.png') }}" class="card-img-top"
+                            alt="...">
+                        <div class="card-body">
+                            <div class="pb-2" style="color: #E3A100; font-size: 12px">23 Februari 2024</div>
+
+                            <h5 class="card-title">
+                                BIMBINGAN TEKNIS PROFESI KEPAMONGPRAJAAN BAGI CAMAT KABUPATEN GARUT
+                            </h5>
+
+                            <p class="card-text" style="font-size: 16px; text-align: justify;">
+                                Garut - Wakil Rektor Bidang Akademik dan Inovasi bersama Direktur dan Sekretaris Program
+                                Studi Profesi menjadi narasumber dalam kegiatan Bimbingan Teknis Profesi Kepamongprajaan
+                                bagi Camat Kabupaten Garut sebanyak 42 Camat.
+                            </p>
+                            <a href="#" class="btn btn-primary">Baca Selengkapnya</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {{-- Kegiatan 3 --}}
+            <div class="row my-4">
+                <div class="col-12">
+                    <div class="card shadow" style="">
+                        <img src="{{ asset('assets/homepage/images/berita/kegiatan/3.png') }}" class="card-img-top"
+                            alt="...">
+                        <div class="card-body">
+                            <div class="pb-2" style="color: #E3A100; font-size: 12px">21 Februari 2024</div>
+
+                            <h5 class="card-title">
+                                PENERIMAAN MAHASISWA BARU PROFESI KEPAMONGPRAJAAN ANGKATAN XIII TAHUN AKADEMIK 2024/2025
+                            </h5>
+
+                            <p class="card-text" style="font-size: 16px; text-align: justify;">
+                                Jakarta - Sebanyak 36 calon mahasiswa profesi kepamongprajaan Angkatan XIII melakukan
+                                pendaftaran ulang dan tes wawancara di Sekretariat Profesi Kepamongprajaan.
+                            </p>
+                            <a href="#" class="btn btn-primary">Baca Selengkapnya</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {{-- Kegiatan 4 --}}
+            <div class="row my-4">
+                <div class="col-12">
+                    <div class="card shadow" style="">
+                        <img src="{{ asset('assets/homepage/images/berita/kegiatan/4.png') }}" class="card-img-top"
+                            alt="...">
+                        <div class="card-body">
+                            <div class="pb-2" style="color: #E3A100; font-size: 12px">21 Februari 2024</div>
+
+                            <h5 class="card-title">
+                                KEGIATAN PENGANUGERAHAN TANDA KARTIKA PAMONG PRAJA
+                            </h5>
+
+                            <p class="card-text" style="font-size: 16px; text-align: justify;">
+                                Jatingagor, 27/10/2023 - Dalam rangka penganugerahan Tanda Kartika Pamong Praja dan
+                                Alumni Kehormatan Kepada Pejabat Gubernur Papua Barat Daya Dr. Drs. Mohammad Musa’ad,
+                                M.Si dan Bupati Malang Drs. H. Sanusi, M.M. sebagai bentuk apresiasi yang tinggi atas
+                                kontribusinya dalam mendayagunakan alumni Institut Pemerintahan Dalam Negeri di Prov.
+                                Papua Barat Daya dan Kab. Malang.
+                            </p>
+                            <a href="#" class="btn btn-primary">Baca Selengkapnya</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {{-- Kegiatan 5 --}}
+            <div class="row my-4">
+                <div class="col-12">
+                    <div class="card shadow" style="">
+                        <img src="{{ asset('assets/homepage/images/berita/kegiatan/5.png') }}" class="card-img-top"
+                            alt="...">
+                        <div class="card-body">
+                            <div class="pb-2" style="color: #E3A100; font-size: 12px">06 Oktober 2023</div>
+
+                            <h5 class="card-title">
+                                Prov. Papua Barat Daya Tingkatkan SDM Kepala Distrik Melalui Pendidikan Profesi
+                                Kepamongprajaan di IPDN -Kemendagri
+                            </h5>
+
+                            <p class="card-text" style="font-size: 16px; text-align: justify;">
+                                Jakarta, 06/10/2023 - Rektor IPDN, Prof. Dr. Drs. H. Hadi Prabowo., M.M resmi membuka
+                                kegiatan matrikulasi dan perkuliahan program pendidikan profesi kepamongprajaan angkatan
+                                XII kelas bratha tahun akademik 2023/2024 dengan 40 Kepala Distrik dari Provinsi Papua
+                                Barat Daya.
+                            </p>
+                            <a href="#" class="btn btn-primary">Baca Selengkapnya</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {{-- Kegiatan 6 --}}
+            <div class="row my-4">
+                <div class="col-12">
+                    <div class="card shadow" style="">
+                        <img src="{{ asset('assets/homepage/images/berita/kegiatan/6.png') }}" class="card-img-top"
+                            alt="...">
+                        <div class="card-body">
+                            <div class="pb-2" style="color: #E3A100; font-size: 12px">21 Februari 2024</div>
+
+                            <h5 class="card-title">
+                                PENGANUGERAHAN TANDA KEHORMATAN DAM LENCANA ALUMNI KEHORMATAN
+                            </h5>
+
+                            <p class="card-text" style="font-size: 16px; text-align: justify;">
+                                Jatingagor 04/03/2023 - Rektor IPDN, Prof. Dr. Drs. H. Hadi Prabowo., M.M memberikan
+                                penganugerahan tanda kehormatan Kartika Pamong Praja Madya, Kartika Pamong Praja Muda
+                                dan Lencana Alumni Kehormatan Pendidikan Tinggi Kepamongprajaan kepada Direktur PPPKp
+                                Dr. Dra. Hj. Endang Try Setyasih.
+                            </p>
+                            <a href="#" class="btn btn-primary">Baca Selengkapnya</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+        {{-- Search --}}
+        <div class="col-3 mx-3 my-4">
+            <div class="row border shadow rounded">
+                <div class="col-12">
+
+                    <div class="mb-2 pt-3">
+
+                        <label for="exampleFormControlInput1" class="form-label"><b>PENCARIAN KEGIATAN</b></label>
+                        <input type="email" class="form-control" id="exampleFormControlInput1"
+                            placeholder="Masukan kata kunci...." />
+                    </div>
+
+                    <a href="#" class="btn btn-primary mb-3 px-4">Cari</a>
+
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
