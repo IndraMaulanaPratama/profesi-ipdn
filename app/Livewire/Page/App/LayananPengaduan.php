486f8d16d66d8891a607f0e1d5d6e8cee9a61bc8
IndraMaulanaPratama <maulana.indra808@gmail.com>
2024-04-29 11:37:22 +0700

    UPDATE || Add session alert on logic page layanan pengaduan

--- app/Livewire/Page/App/LayananPengaduan.php
+++ app/Livewire/Page/App/LayananPengaduan.php
@@ -2,12 +2,38 @@
 
 namespace App\Livewire\Page\App;
 
+use Livewire\Attributes\On;
 use Livewire\Attributes\Title;
 use Livewire\Component;
 
 #[Title("Profesi - Layanan Pengaduan")]
+
 class LayananPengaduan extends Component
 {
+    #[On("success")]
+    public function processSuccessfully($message)
+    {
+        session()->reflash();
+        session()->flash('success', $message);
+    }
+
+
+
+    #[On("warning")]
+    public function warningProcess($message)
+    {
+        session()->reflash();
+        session()->flash('warning', $message);
+    }
+
+
+    #[On("error")]
+    public function errorProcess($message)
+    {
+        session()->reflash();
+        session()->flash('error', $message);
+    }
+
     public function render()
     {
         return view('livewire.page.app.layanan-pengaduan');
