c50f72feca93f8e7256c8b67f52ca73215edb7fd
IndraMaulanaPratama <maulana.indra808@gmail.com>
2024-04-29 11:42:17 +0700

    UPDATE || Move session alert on layanan pengaduan page

--- resources/views/livewire/page/app/layanan-pengaduan.blade.php
+++ resources/views/livewire/page/app/layanan-pengaduan.blade.php
@@ -1,21 +1,25 @@
 {{-- Close your eyes. Count to one. That is how long forever feels. --}}
 
 <div>
-    @if (session('success'))
-        <x-admin.components.alert.success text="{{ session('success') }}" />
-    @endif
-
-    @if (session('warning'))
-        <x-admin.components.alert.warning text="{{ session('warning') }}" />
-    @endif
-
-    @if (session('error'))
-        <x-admin.components.alert.error text="{{ session('error') }}" />
-    @endif
 
     {{-- Header --}}
     @livewire('App.LayananPengaduan.Header', [], key('header'))
 
+    <div class="m-4">
+
+        @if (session('success'))
+            <x-admin.components.alert.success text="{{ session('success') }}" />
+        @endif
+
+        @if (session('warning'))
+            <x-admin.components.alert.warning text="{{ session('warning') }}" />
+        @endif
+
+        @if (session('error'))
+            <x-admin.components.alert.error text="{{ session('error') }}" />
+        @endif
+
+    </div>
 
     {{-- Content --}}
     @livewire('App.LayananPengaduan.Content', [], key('content'))
