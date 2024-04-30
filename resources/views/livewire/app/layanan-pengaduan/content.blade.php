399618463cb2acc36ea2eecd05bc8fc9681640c5
IndraMaulanaPratama <maulana.indra808@gmail.com>
2024-04-30 10:34:52 +0700

    Fixing bug || file upload pengaduan

--- resources/views/livewire/app/layanan-pengaduan/content.blade.php
+++ resources/views/livewire/app/layanan-pengaduan/content.blade.php
@@ -52,7 +52,7 @@
                     <div class="row justify-content-between">
 
                         {{-- FORM PENGADUAN --}}
-                        <div class="col-5 px-5 py-4">
+                        <div class="col-5 px-5 py-4" wire:ignore.self>
                             <form wire:submit.prevent='buatPengajuan' method="POST">
                                 {{-- Email --}}
                                 <div class="py-2">
