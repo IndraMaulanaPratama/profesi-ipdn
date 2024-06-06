d4ad966d337d2b0ac8740dfe02d83004a5386818
IndraMaulanaPratama <maulana.indra808@gmail.com>
2024-06-06 23:31:29 +0700

    FIXING BUG || Footer modal component

--- resources/views/components/admin/components/modal/footer.blade.php
+++ resources/views/components/admin/components/modal/footer.blade.php
@@ -1,5 +1,7 @@
 <div class="modal-footer">
     <button type="button" wire:click='resetForm' class="btn btn-outline-secondary"
         data-bs-dismiss="modal">Batalkan</button>
-    <x-admin.components.form.button type='submit' color='primary' text="Ubah Data" />
+
+    <button type="submit"' class="btn btn-outline-primary"
+        data-bs-dismiss="modal">Simpan</button>
 </div>
