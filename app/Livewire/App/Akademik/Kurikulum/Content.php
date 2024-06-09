44c132f1c1015f2f010f90c19a7aa39d79cc54ac
IndraMaulanaPratama <maulana.indra808@gmail.com>
2024-06-09 20:29:49 +0700

    UPDATE || Implement dynamic data on app kurikulum

--- app/Livewire/App/Akademik/Kurikulum/Content.php
+++ app/Livewire/App/Akademik/Kurikulum/Content.php
@@ -2,12 +2,22 @@
 
 namespace App\Livewire\App\Akademik\Kurikulum;
 
+use App\Models\Kurikulum;
 use Livewire\Component;
 
 class Content extends Component
 {
+
+
     public function render()
     {
-        return view('livewire.app.akademik.kurikulum.content');
+
+        $semester1 = Kurikulum::where('KURIKULUM_SEMESTER', '=', 1)->get();
+        $semester2 = Kurikulum::where('KURIKULUM_SEMESTER', '=', 2)->get();
+
+        return view('livewire.app.akademik.kurikulum.content', [
+            'semester1' => $semester1,
+            'semester2' => $semester2,
+        ]);
     }
 }
