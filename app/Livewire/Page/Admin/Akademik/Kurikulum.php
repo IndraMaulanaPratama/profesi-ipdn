8f3da48d9e88ef3675bfda705e4e3f57e43d7ced
IndraMaulanaPratama <maulana.indra808@gmail.com>
2024-06-09 20:30:15 +0700

    ADD NEW || Logic for page kurikulum

---
+++ app/Livewire/Page/Admin/Akademik/Kurikulum.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Livewire\Page\Admin\Akademik;
+
+use Livewire\Attributes\Layout;
+use Livewire\Attributes\On;
+use Livewire\Attributes\Title;
+use Livewire\Component;
+
+class Kurikulum extends Component
+{
+
+    #[Title('Kurikulum')]
+    #[Layout("layouts/admin")]
+
+
+    #[On("success")]
+    public function processSuccessfully($message)
+    {
+        session()->reflash();
+        session()->flash('success', $message);
+    }
+
+
+
+    #[On("warning")]
+    public function warningProcess($message)
+    {
+        session()->reflash();
+        session()->flash('warning', $message);
+    }
+
+
+    #[On("error")]
+    public function errorProcess($message)
+    {
+        session()->reflash();
+        session()->flash('error', $message);
+    }
+
+    public function render()
+    {
+        return view('livewire.page.admin.akademik.kurikulum');
+    }
+}
