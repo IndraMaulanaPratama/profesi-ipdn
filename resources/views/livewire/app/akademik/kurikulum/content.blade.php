990e2ffbbbce72593a6de5448e481c5eaa2097a7
IndraMaulanaPratama <maulana.indra808@gmail.com>
2024-06-09 20:32:27 +0700

    UPDATE || Implement dynamic data for kurikulum page

--- resources/views/livewire/app/akademik/kurikulum/content.blade.php
+++ resources/views/livewire/app/akademik/kurikulum/content.blade.php
@@ -15,65 +15,22 @@
 
 
         {{-- Table Semester 1 --}}
-        <div class="table-responsive">
-            <table class="table">
+        <div class="table-responsive ">
+            <table class="table table-hover">
                 <tr>
                     <th style="width: 5cm">KODE</th>
                     <th>MATA KULIAH</th>
                     <th style="width: 2.5cm">SKS</th>
                 </tr>
 
+                @foreach ($semester1 as $item)
+                    <tr>
+                        <td> {{ $item['KURIKULUM_KODE'] }} </td>
+                        <td> {{ $item['KURIKULUM_MATAKULIAH'] }} </td>
+                        <td> {{ $item['KURIKULUM_SKS'] }} </td>
+                    </tr>
+                @endforeach
 
-                <tr>
-                    <td>PKP230101</td>
-                    <td>Teknik Kepemimpinan Kepamongprajaan</td>
-                    <td>3</td>
-                </tr>
-                <tr>
-                    <td>PKP230102</td>
-                    <td>Teknik Administrasi Pemerintahan Daerah</td>
-                    <td>2</td>
-                </tr>
-                <tr>
-                    <td>PKP230103</td>
-                    <td>Teknik Analisis Potensi Wilayah dan Perumusan Kebijakan Pemerintahan</td>
-                    <td>2</td>
-                </tr>
-                <tr>
-                    <td>PKP230104</td>
-                    <td>Teknik Adminitrasi Pertanahan</td>
-                    <td>2</td>
-                </tr>
-                <tr>
-                    <td>PKP230105</td>
-                    <td>Teknik Perencanaan dan Pengawasan Pelaksanaan Pembangunan</td>
-                    <td>2</td>
-                </tr>
-                <tr>
-                    <td>PKP230106</td>
-                    <td>Teknik Pengelolaan Keuangan Daerah</td>
-                    <td>2</td>
-                </tr>
-                <tr>
-                    <td>PKP230107</td>
-                    <td>Teknik Manajemen Pembangunan dan Pemberdayaan Masyarakat</td>
-                    <td>2</td>
-                </tr>
-                <tr>
-                    <td>PKP230108</td>
-                    <td>Teknik Pelayanan Publik Berdasarkan Standar Pelayanan Minimal</td>
-                    <td>2</td>
-                </tr>
-                <tr>
-                    <td>PKP230109</td>
-                    <td>Teknik Pembinaan Ketentraman dan Ketertiban Umum</td>
-                    <td>2</td>
-                </tr>
-                <tr>
-                    <td>PKP230110</td>
-                    <td>Teknik Penulisan Laporan Praktik</td>
-                    <td>2</td>
-                </tr>
             </table>
         </div>
 
@@ -93,24 +50,20 @@
 
         {{-- Table Semester 2 --}}
         <div class="table-responsive">
-            <table class="table">
+            <table class="table table-hover">
                 <tr>
                     <th style="width: 5cm">KODE</th>
                     <th>MATA KULIAH</th>
                     <th style="width: 2.5cm">SKS</th>
                 </tr>
 
-
-                <tr>
-                    <td>PK230201</td>
-                    <td>Praktik Lapangan</td>
-                    <td>2</td>
-                </tr>
-                <tr>
-                    <td>PK230202</td>
-                    <td>Laporan Praktik Lapangan</td>
-                    <td>4</td>
-                </tr>
+                @foreach ($semester2 as $item)
+                    <tr>
+                        <td> {{ $item['KURIKULUM_KODE'] }} </td>
+                        <td> {{ $item['KURIKULUM_MATAKULIAH'] }} </td>
+                        <td> {{ $item['KURIKULUM_SKS'] }} </td>
+                    </tr>
+                @endforeach
 
             </table>
         </div>
