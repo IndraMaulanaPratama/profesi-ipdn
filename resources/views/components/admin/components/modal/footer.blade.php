5c01c20050716ec82fe0fa2a44de337316c1a9a2
IndraMaulanaPratama <maulana.indra808@gmail.com>
2024-06-10 08:52:40 +0700

    UPDATE || Removing unused code on component footer modal

--- resources/views/components/admin/components/modal/footer.blade.php
+++ resources/views/components/admin/components/modal/footer.blade.php
@@ -2,6 +2,6 @@
     <button type="button" wire:click='resetForm' class="btn btn-outline-secondary"
         data-bs-dismiss="modal">Batalkan</button>
 
-    <button type="submit"' class="btn btn-outline-primary"
+    <button type="submit" class="btn btn-outline-primary"
         data-bs-dismiss="modal">Simpan</button>
 </div>
