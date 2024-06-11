44f3e72cc381a613c0b5b6ef8c29bfc51573f954
IndraMaulanaPratama <maulana.indra808@gmail.com>
2024-06-12 00:39:06 +0700

    UPDATE || Implement dynamic data for logic kalender akacemik page

--- app/Livewire/Page/App/Akademik/KalenderAkademik.php
+++ app/Livewire/Page/App/Akademik/KalenderAkademik.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire\Page\App\Akademik;
 
+use Carbon\Carbon;
 use Livewire\Attributes\Title;
 use Livewire\Component;
 
@@ -11,6 +12,20 @@ class KalenderAkademik extends Component
 {
     public function render()
     {
-        return view('livewire.page.app.akademik.kalender-akademik');
+        $tahunAjaran = \App\Models\KalenderAkademik::groupBy('KALENDER_TAHUN_AJARAN')->get('KALENDER_TAHUN_AJARAN as tahun');
+
+        for ($i = 0; $i < count($tahunAjaran); $i++) {
+            $jumlahKegiatanTahunan[$i] = \App\Models\KalenderAkademik::where('KALENDER_TAHUN_AJARAN', $tahunAjaran[$i]['tahun'])->count('KALENDER_ID');
+            $jumlahKegiatanSemester[$i] = \App\Models\KalenderAkademik::where('KALENDER_SEMESTER', $i + 1)->count('KALENDER_ID');
+        }
+
+        $data = \App\Models\KalenderAkademik::orderBy('KALENDER_URUTAN')->get();
+
+        return view('livewire.page.app.akademik.kalender-akademik', [
+            'data' => $data,
+            'jumlah_kegiatan_tahunan' => $jumlahKegiatanTahunan,
+            'jumlah_kegiatan_semester' => $jumlahKegiatanSemester,
+            'tahun_ajaran' => $tahunAjaran,
+        ]);
     }
 }
