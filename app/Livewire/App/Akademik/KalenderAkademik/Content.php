2b793735b327e20fae6f569dd40c883e5fd22ec1
IndraMaulanaPratama <maulana.indra808@gmail.com>
2024-06-20 14:30:59 +0700

    UPDATE || Logic for content kalender akademik page

--- app/Livewire/App/Akademik/KalenderAkademik/Content.php
+++ app/Livewire/App/Akademik/KalenderAkademik/Content.php
@@ -2,26 +2,20 @@
 
 namespace App\Livewire\App\Akademik\KalenderAkademik;
 
+use App\Models\KalenderAkademik;
 use Livewire\Component;
 
 class Content extends Component
 {
     public function render()
     {
-        $tahunAjaran = \App\Models\KalenderAkademik::groupBy('KALENDER_TAHUN_AJARAN')->get('KALENDER_TAHUN_AJARAN as tahun');
 
-        for ($i = 0; $i < count($tahunAjaran); $i++) {
-            $jumlahKegiatanTahunan[$i] = \App\Models\KalenderAkademik::where('KALENDER_TAHUN_AJARAN', $tahunAjaran[$i]['tahun'])->count('KALENDER_ID');
-            $jumlahKegiatanSemester[$i] = \App\Models\KalenderAkademik::where('KALENDER_SEMESTER', $i + 1)->count('KALENDER_ID');
-        }
-
-        $data = \App\Models\KalenderAkademik::orderBy('KALENDER_URUTAN')->get();
+        $semester1 = KalenderAkademik::where('KALENDER_SEMESTER', 1)->orderBy('KALENDER_URUTAN')->get();
+        $semester2 = KalenderAkademik::where('KALENDER_SEMESTER', 2)->orderBy('KALENDER_URUTAN')->get();
 
         return view('livewire.app.akademik.kalender-akademik.content', [
-            'data' => $data,
-            'jumlah_kegiatan_tahunan' => $jumlahKegiatanTahunan,
-            'jumlah_kegiatan_semester' => $jumlahKegiatanSemester,
-            'tahun_ajaran' => $tahunAjaran,
+            'semester1' => $semester1,
+            'semester2' => $semester2,
         ]);
     }
 }
