0e7875b95a639f6c41ca8960e4dbc2e5ac996076
IndraMaulanaPratama <maulana.indra808@gmail.com>
2024-06-26 09:56:20 +0700

    ADD NEW || Logic for create pelatihan laboratorium

---
+++ app/Livewire/Admin/Akademik/Laboratorium/Pelatihan/Create.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Livewire\Admin\Akademik\Laboratorium\Pelatihan;
+
+use App\Models\LaboratoriumPelatihan;
+use Illuminate\Support\Facades\Auth;
+use Livewire\Attributes\Rule;
+use Livewire\Component;
+
+class Create extends Component
+{
+    #[Rule(['required', 'string', 'max:255'])]
+    public $inputJudul;
+
+    #[Rule(['required', 'string'])]
+    public $inputDeskripsi;
+
+    #[Rule(['required', 'string'])]
+    public $inputTautan;
+
+
+    /**
+     * Fungsi kanggo mulangkeun kondisi form sakumaha bahara-bahari
+     * 
+     */
+    public function resetForm()
+    {
+        $this->reset();
+    }
+
+
+    /**
+     * Fungsi kanggo nambih data
+     * 
+     * @inputJudul, @inputDeskripsi
+     */
+    public function createData()
+    {
+        $this->validate();
+
+        try {
+
+            // Nangtoskeun urutan
+            $urutan = LaboratoriumPelatihan::where('PELATIHAN_KATEGORI', 'pelatihan')->max('PELATIHAN_URUTAN');
+
+            // Inisialisasi data tabel
+            $data = [
+                'PELATIHAN_ID' => uuid_create(4),
+                'PELATIHAN_JUDUL' => $this->inputJudul,
+                'PELATIHAN_DESKRIPSI' => $this->inputDeskripsi,
+                'PELATIHAN_TAUTAN' => $this->inputTautan,
+                'PELATIHAN_KATEGORI' => 'pelatihan',
+                'PELATIHAN_OFFICER' => Auth::user()->id,
+                'PELATIHAN_URUTAN' => $urutan + 1,
+            ];
+
+            // Proses ngalebetkeun data ka tabel
+            LaboratoriumPelatihan::create($data);
+
+            // Mulangkeun kondisi form sakumaha bahara-bahari
+            $this->reset();
+
+            // Ngadamel bewara yen data parantos ditambih
+            $this->dispatch('success', 'Data pelatihan berhasil ditambahkan');
+
+        } catch (\Throwable $th) {
+            // Ngadamel bewara yen data gagal ditambih
+            $this->dispatch('warning', 'Data pelatihan gagal ditambahkan');
+            // $this->dispatch('warning', $th->getMessage());
+
+        }
+    }
+
+
+    public function render()
+    {
+        return view('livewire.admin.akademik.laboratorium.pelatihan.create');
+    }
+}
