f9179449d617a6e444290d38d2ace7cf93bd417d
IndraMaulanaPratama <maulana.indra808@gmail.com>
2024-06-29 17:16:15 +0700

    UPDATE || change input deskripsi to editor on add new pelatihan

--- resources/views/livewire/admin/akademik/laboratorium/pelatihan/create.blade.php
+++ resources/views/livewire/admin/akademik/laboratorium/pelatihan/create.blade.php
@@ -1,8 +1,6 @@
 {{-- Because she competes with no one, no one can compete with her. --}}
 
-<div class="container">
-
-    <x-admin.components.modal.header id="formCreate" title='Tambah data pelatihan' />
+<div class="">
 
     <form wire:submit.prevent='createData' method="POST">
         <div class="row mt-3 g-3">
@@ -12,22 +10,40 @@
                 <x-admin.components.form.input name='inputJudul' placeholder='Judul pelatihan' />
             </div>
 
-
-            {{-- Input Kode --}}
-            <div class="">
-                <x-admin.components.form.textarea name='inputDeskripsi' placeholder='Deksripsi' />
+            {{-- Input Deskripsi --}}
+            <div class="" wire:ignore>
+                <textarea id='input'></textarea>
             </div>
 
-            {{-- Input Judul --}}
-            <div class="">
-                <x-admin.components.form.input name='inputTautan' placeholder='Tautan pelatihan' />
+            {{-- Tombol submit sareng reset --}}
+            <div class="text-end">
+                <!-- Tombol Reset -->
+                <a href="{{ route('admin.akademik.laboratorium.pelatihan') }}"
+                    class="btn btn-outline-secondary">Kembali</a>
+
+                <!-- Tombol Submit -->
+                <x-admin.components.form.button type="submit" id="btnSubmit" color="primary" text="simpan" />
             </div>
-
-
-
-            <x-admin.components.modal.footer />
 
         </div>
     </form>
 
 </div>
+
+<script>
+    $(document).ready(function() {
+        const editor = CKEDITOR.replace('input');
+        editor.on('change', function(event) {
+            @this.set('inputDeskripsi', event.editor.getData());
+        });
+
+        $("#btnSubmit").click(function() {
+            CKEDITOR.instances['input'].setData("");
+        });
+
+        $("#btnReset").click(function() {
+            CKEDITOR.instances['input'].setData("");
+        });
+
+    });
+</script>
