ed24d2d6710e5a8249abcfc70c9c5e6cf2aa2486
IndraMaulanaPratama <maulana.indra808@gmail.com>
2024-06-29 17:36:30 +0700

    UPDATE || Create dynamic data to front end on logic laboratorium

--- app/Livewire/App/Akademik/Laboratorium/Content.php
+++ app/Livewire/App/Akademik/Laboratorium/Content.php
@@ -2,12 +2,22 @@
 
 namespace App\Livewire\App\Akademik\Laboratorium;
 
+use App\Models\LaboratoriumLayanan;
+use App\Models\LaboratoriumPelatihan;
 use Livewire\Component;
 
 class Content extends Component
 {
     public function render()
     {
-        return view('livewire.app.akademik.laboratorium.content');
+        $layanan = LaboratoriumLayanan::orderBy('LAYANAN_URUTAN')->get();
+        $pelatihan = LaboratoriumPelatihan::where('PELATIHAN_KATEGORI', 'pelatihan')->orderBy('PELATIHAN_URUTAN')->get();
+        $sumber = LaboratoriumPelatihan::where('PELATIHAN_KATEGORI', 'sumber')->orderBy('PELATIHAN_URUTAN')->get();
+
+        return view('livewire.app.akademik.laboratorium.content', [
+            'layanan' => $layanan,
+            'pelatihan' => $pelatihan,
+            'sumber' => $sumber,
+        ]);
     }
 }
