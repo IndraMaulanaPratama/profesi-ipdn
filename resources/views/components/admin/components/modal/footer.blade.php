2d97e55d18bf1220ef01618b86a880206a01fa33
IndraMaulanaPratama <maulana.indra808@gmail.com>
2024-06-29 14:43:42 +0700

    UPDATE || Add parameter id on modal footer

--- resources/views/components/admin/components/modal/footer.blade.php
+++ resources/views/components/admin/components/modal/footer.blade.php
@@ -1,7 +1,7 @@
 <div class="modal-footer">
-    <button type="button" wire:click='resetForm' class="btn btn-outline-secondary"
+    <button type="button" wire:click='resetForm' id="{{ $idReset ?? null }}" class="btn btn-outline-secondary"
         data-bs-dismiss="modal">Batalkan</button>
 
-    <button type="submit" class="btn btn-outline-primary"
+    <button type="submit" id="{{ $idSubmit ?? null }}" class="btn btn-outline-primary"
         data-bs-dismiss="modal">Simpan</button>
 </div>
