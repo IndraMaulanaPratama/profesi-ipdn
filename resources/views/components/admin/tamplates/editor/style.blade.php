325e8ba20c6efe06c32732eb2cc1a3c729e2ae1d
IndraMaulanaPratama <maulana.indra808@gmail.com>
2024-08-05 16:08:59 +0700

    ADD NEW || Style for CKE editor on component

---
+++ resources/views/components/admin/tamplates/editor/style.blade.php
@@ -0,0 +1,4 @@
+<script src="https://code.jquery.com/jquery-3.7.1.slim.js"
+integrity="sha256-UgvvN8vBkgO0luPSUl2s8TIlOSYRoGFAX4jlCIm9Adc=" crossorigin="anonymous"></script>
+
+<script src="https://cdn.ckeditor.com/4.22.1/standard/ckeditor.js"></script>
